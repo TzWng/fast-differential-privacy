9526ca60438eb34e8910dfba75b85068bf16b20e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-06-04 22:42:19 -0400

    Update __init__.py

--- fastDP/__init__.py
+++ fastDP/__init__.py
@@ -1,4 +1,5 @@
 from . import lora_utils
 from .privacy_engine import PrivacyEngine
+from .privacy_engine_dist_extending import PrivacyEngine_Distributed_extending
 from .supported_differentially_private_layers import *
 __version__ = '2.0.0'
