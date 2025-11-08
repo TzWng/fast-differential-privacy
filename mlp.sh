a55a1af294291cf9fa00f74d367c000b566b839e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-08 06:49:22 -0500

    Create mlp.sh

---
+++ mlp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PYTHON=python
+BS=1024  # 你要的 batch size
+
+# 手写一组 log2lr，对应 2^{lr}
+LRS=(-8 -7.5 -7 -6.5 -6 -5.5 -5)
+
+for wid in 256 512 1024 2048 4096; do
+  for lr in "${LRS[@]}"; do
+    $PYTHON MLP_unifed.py \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_clipping_only.txt"
+  done
+done
