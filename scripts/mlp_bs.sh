6a73949afbb70052756b308a3e22b8ef1ba09480
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-09 15:56:29 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -3,18 +3,14 @@
 PYTHON=python3.10
 BS=1024  # 你要的 batch size
 
-LRS=(-8 -7.5 -7 -6.5 -6 -5.5 -5)
-# LRS=(-5 -4.5 -4 -3.5 -3 -2.5 -2 -1.5 -1 -0.5 0 0.5 1 1.5 2)
-# LRS=(-2 -1.5 -1 -0.5 0 0.5 1)
-# LRS=(-6 -5.5 -5 -4.5 -4 -3.5 -3)
-# LRS=(-4 -3.5 -3 -2.5 -2 -1.5 -1)
-# LRS=(1.5 2)
+LRS=(-7 -6.5 -6 -5.5 -5 -4.5 -4 -3.5)
+LRS=(-4.5 -4 -3.5)
 
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-for BS in 125 250 500 1000 2000; do
+for BS in 250 500 1000 2000; do
   epoch=$(( 5 * BS / 125 ))
   for lr in "${LRS[@]}"; do
     $PYTHON -m scripts.MLP_unifed \
