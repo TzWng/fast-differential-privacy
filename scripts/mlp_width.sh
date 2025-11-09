1dfd01336deaeebce4825bc2649bd936ef1b17f3
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-09 15:52:31 -0500

    Create mlp_width.sh

---
+++ scripts/mlp_width.sh
@@ -0,0 +1,21 @@
+
+
+
+for wid in 256 512 1024 2048 4096 8192; do
+  for lr in "${LRS[@]}"; do
+    $PYTHON -m scripts.MLP_unifed \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20\
+      --bs 2000 \
+      --mini_bs 2000 \
+      --epsilon 2 \
+      --noise 1 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_sig10.txt"
+  done
+done
