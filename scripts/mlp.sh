8a616d7a236593e4e38151e78d10d32b4c2b529b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-09 15:13:28 -0500

    Update mlp.sh

--- scripts/mlp.sh
+++ scripts/mlp.sh
@@ -3,9 +3,9 @@
 PYTHON=python3.10
 BS=1024  # 你要的 batch size
 
-# LRS=(-8 -7.5 -7 -6.5 -6 -5.5 -5 -4.5 -4 -3.5 -3)
+LRS=(-8 -7.5 -7 -6.5 -6 -5.5 -5 -4.5 -4 -3.5 -3)
 # LRS=(-5 -4.5 -4 -3.5 -3 -2.5 -2 -1.5 -1 -0.5 0 0.5 1 1.5 2)
-LRS=(-2 -1.5 -1 -0.5 0 0.5 1)
+# LRS=(-2 -1.5 -1 -0.5 0 0.5 1)
 # LRS=(-5 -4.5 -4 -3.5 -3)
 # LRS=(-4 -3.5 -3 -2.5 -2 -1.5 -1)
 # LRS=(1.5 2)
@@ -14,34 +14,15 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-for BS in 125 250 500 1000 2000; do
-  epoch=$(( 5 * BS / 125 ))
-  for lr in "${LRS[@]}"; do
-    $PYTHON -m scripts.MLP_unifed \
-      --width 4096 \
-      --lr "$lr" \
-      --epochs "$epoch"\
-      --bs "$BS" \
-      --mini_bs "$BS" \
-      --epsilon 2 \
-      --noise 0.5 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_sig05.txt"
-  done
-done
-
-# for BS in 256 512 1024 2048 4096 8192; do
+# for BS in 125 250 500 1000 2000; do
+#   epoch=$(( 5 * BS / 125 ))
 #   for lr in "${LRS[@]}"; do
 #     $PYTHON -m scripts.MLP_unifed \
 #       --width 4096 \
 #       --lr "$lr" \
-#       --epochs 20\
-#       --bs 2000 \
-#       --mini_bs 2000 \
+#       --epochs "$epoch"\
+#       --bs "$BS" \
+#       --mini_bs "$BS" \
 #       --epsilon 2 \
 #       --noise 0.5 \
 #       --clipping_mode BK-MixOpt \
@@ -52,3 +33,22 @@ done
 #       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_sig05.txt"
 #   done
 # done
+
+for wid in 256 512 1024 2048 4096 8192; do
+  for lr in "${LRS[@]}"; do
+    $PYTHON -m scripts.MLP_unifed \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20\
+      --bs 2000 \
+      --mini_bs 2000 \
+      --epsilon 2 \
+      --noise 0.5 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_sig05.txt"
+  done
+done
