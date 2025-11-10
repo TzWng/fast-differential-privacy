a03ffaa598bfff30587a0f284aa5f164ff017f90
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-10 13:11:48 -0500

    Create vit_unifed.py

---
+++ scripts/vit_unifed.py
@@ -0,0 +1,222 @@
+'''Train CIFAR10/CIFAR100 with PyTorch.'''
+from .logger import ExecutionLogger
+import numpy as np
+
+def main(args):
+    if args.clipping_mode not in ['nonDP', 'BK-ghost', 'BK-MixGhostClip', 'BK-MixOpt', 'nonDP-BiTFiT', 'BiTFiT']:
+        print("Mode must be one of 'nonDP','BK-ghost', 'BK-MixGhostClip', 'BK-MixOpt','nonDP-BiTFiT','BiTFiT'")
+        return None
+
+    device = torch.device("cuda:0")
+
+    # Data
+    print('==> Preparing data..')
+
+    transformation = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(args.dimension),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    if args.cifar_data == 'CIFAR10':
+        trainset = torchvision.datasets.CIFAR10(root='data/', train=True, download=True, transform=transformation)
+        testset = torchvision.datasets.CIFAR10(root='data/', train=False, download=True, transform=transformation)
+    elif args.cifar_data == 'CIFAR100':
+        trainset = torchvision.datasets.CIFAR100(root='data/', train=True, download=True, transform=transformation)
+        testset = torchvision.datasets.CIFAR100(root='data/', train=False, download=True, transform=transformation)
+    else:
+        return "Must specify datasets as CIFAR10 or CIFAR100"
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=args.mini_bs, shuffle=True, num_workers=4)
+
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=4)
+
+    n_acc_steps = args.bs // args.mini_bs  # gradient accumulation steps
+
+    def kaiming_init_weights(m):
+        if isinstance(m, nn.Linear):
+            nn.init.kaiming_normal_(m.weight, a=1, mode='fan_in')
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+
+    def kaiming_init_weights(m):
+        if isinstance(m, nn.Linear):
+            if m is net.head:
+                nn.init.zeros_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            else:
+                nn.init.kaiming_normal_(m.weight, a=1, mode='fan_in')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+
+
+
+    # Model
+    print('==> Building model..', args.model, '; BatchNorm is replaced by GroupNorm. Mode: ', args.clipping_mode)
+    net = timm.create_model(args.model, pretrained=False, num_classes=int(args.cifar_data[5:]),
+                            embed_dim=int(192 * args.scale), num_heads=int(6 * args.scale), mlp_ratio=4.0)
+    net.apply(kaiming_init_weights)
+    net = ModuleValidator.fix(net)
+    net = net.to(device)
+
+    print('Number of total parameters: ', sum([p.numel() for p in net.parameters()]))
+    print('Number of trainable parameters: ', sum([p.numel() for p in net.parameters() if p.requires_grad]))
+
+    criterion = nn.CrossEntropyLoss()
+
+    base_lr = 2 ** args.lr
+    param_groups = [
+        {"params": [p], "lr": base_lr, "name": n}
+        for n, p in net.named_parameters()
+    ]
+  
+    if args.optimizer == 'SGD':
+        optimizer = optim.SGD(param_groups, lr=base_lr)
+    elif args.optimizer == 'Adam':
+        optimizer = optim.Adam(param_groups, lr=base_lr)
+
+
+    if 'BiTFiT' in args.clipping_mode:  # not needed for DP-BiTFiT but use here for safety
+        for name, param in net.named_parameters():
+            if '.bias' not in name:
+                param.requires_grad_(False)
+
+    # Privacy engine
+    if 'nonDP' not in args.clipping_mode:
+        sigma = get_noise_multiplier(
+            target_epsilon=args.epsilon,
+            target_delta=1e-5,
+            sample_rate=args.bs / len(trainset),
+            epochs=args.epochs,
+        )
+
+        if 'BK' in args.clipping_mode:
+            clipping_mode = args.clipping_mode[3:]
+        else:
+            clipping_mode = 'ghost'
+
+        if args.clipping_style in [['all-layer'], ['layer-wise'], ['param-wise']]:
+            args.clipping_style = args.clipping_style[0]
+        privacy_engine = PrivacyEngine(
+            net,
+            batch_size=args.bs,
+            sample_size=len(trainset),
+            noise_multiplier=sigma,
+            epochs=args.epochs,
+            clipping_mode=clipping_mode,
+            clipping_style=args.clipping_style,
+            origin_params=args.origin_params,  # ['patch_embed.proj.bias'],
+        )
+        privacy_engine.attach(optimizer)
+        print("Noise multiplier (σ):", privacy_engine.noise_multiplier)
+
+    def train(epoch):
+
+        net.train()
+        train_loss = 0
+        correct = 0
+        total = 0
+        eps = 1e-6
+
+
+        for batch_idx, (inputs, targets) in enumerate(tqdm(trainloader)):
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            # forward + loss
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            
+            if ((batch_idx + 1) % n_acc_steps == 0) or ((batch_idx + 1) == len(trainloader)):
+                for group in optimizer.param_groups:
+                    param = group["params"][0]
+                    grad = param.grad
+                    lr_scale = 1.0
+                  
+                    if grad is not None and grad.ndim in (1, 2):
+                        # spec = torch.linalg.norm(grad, ord=2).clamp(min=eps)
+                        # print("spectral norm is", spec)
+                        # spec = (param.shape[0]**0.5 + param.shape[1]**0.5) * sigma
+                        if grad.ndim == 2:
+                            if args.optimizer == 'SGD':
+                                lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 # / spec
+                            elif args.optimizer == 'Adam':
+                                lr_scale = (param.shape[0] / param.shape[1]) ** 0.5   
+                                # lr_scale = 1 / param.shape[1]  
+                        else:
+                            if args.optimizer == 'SGD':
+                                lr_scale = (param.shape[0]) ** 0.5 # / spec
+                            elif args.optimizer == 'Adam':
+                                lr_scale = (param.shape[0]) ** 0.5
+                            
+                    group["lr"] = base_lr * lr_scale
+
+                optimizer.step()
+                optimizer.zero_grad()
+
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+        print('Epoch: ', epoch, len(trainloader), 'Train Loss: %.3f | Acc: %.3f%% (%d/%d)'
+              % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+        return train_loss / (batch_idx + 1)
+
+    for epoch in range(args.epochs):
+        train_loss = train(epoch)
+        if math.isnan(train_loss):
+            break
+
+    logger = ExecutionLogger(args.log_path)
+    logger.log(log2lr=args.lr, train_loss=train_loss, width=args.width, batch=args.bs, sigma=args.noise)
+
+
+
+from fastDP import PrivacyEngine 
+import math, torch, os, torchvision 
+torch.manual_seed(2) 
+import torch.nn as nn 
+import torch.optim as optim 
+import torch.nn.functional as F 
+from torchvision import datasets, transforms 
+from opacus.validators import ModuleValidator 
+from opacus.accountants.utils import get_noise_multiplier 
+from torch import nn 
+from tqdm import tqdm 
+import warnings; 
+warnings.filterwarnings("ignore")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="vit_tiny_patch16_224")
+    parser.add_argument("--width", type=int, default=256)
+    parser.add_argument("--lr", type=float, default=-11.0)  # 和你画图保持一致
+    parser.add_argument("--epochs", type=int, default=20)
+    parser.add_argument("--bs", type=int, default=512)
+    parser.add_argument("--mini_bs", type=int, default=512)
+    parser.add_argument("--epsilon", type=float, default=2.0)
+    parser.add_argument("--clipping_mode", type=str, default="BK-ghost")
+    parser.add_argument("--clipping_style", nargs="+", type=str, default="layer-wise")
+    parser.add_argument("--scale", type=int, default=1)
+    parser.add_argument("--cifar_data", type=str, default="CIFAR10")
+    parser.add_argument("--dimension", type=int, default=32)
+    parser.add_argument("--optimizer", type=str, default="Adam")
+    parser.add_argument("--origin_params", nargs="+", default=None)
+    parser.add_argument(
+        "--log_path",
+        type=str,
+        default="/content/drive/MyDrive/DP_muP/logs/MLP_Adam_DP_noise.txt",
+    )
+
+
+    args = parser.parse_args()
+    torch.manual_seed(2)
+    main(args)
