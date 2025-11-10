ceb02e80ec25eebfbe34298da86733948c70dc5a
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-10 11:06:24 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -6,7 +6,7 @@ BS=1024  # 你要的 batch size
 LRS=(-13 -12.5 -12 -11.5 -11 -10.5 -10)
 LRS=(-8) # 500
 LRS=(-7.5 -7) #1000
-# LRS=(-6 -5.5 -11 -11.5 -12) #2000
+# LRS=(-6 -11 -11.5 -12) #2000
 
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
