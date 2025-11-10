4dffc6d7f579da0c3b73763752b3238f57e1cfb5
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-10 14:42:00 -0500

    Create vit_SGD.sh

---
+++ scripts/vit_SGD.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PYTHON=python3.10
+
+PROJECT_ROOT=/content/fast-differential-privacy
+export PYTHONPATH="$PROJECT_ROOT"
+
+LRS=(-2 -1.5 -1 -0.5 0)
+
+MODELS=(
+  vit_tiny_patch16_224
+  vit_small_patch16_224
+  vit_base_patch16_224
+  vit_large_patch16_224
+)
+
+for s in 1 2 3 4 5; do
+  for lr in "${LRS[@]}"; do
+    $PYTHON -m scripts.vit_unifed \
+      --lr "$lr" \
+      --epochs 3\
+      --bs 200 \
+      --mini_bs 200 \
+      --epsilon 2 \
+      --noise 1 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 224 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_SGD_diffwidth_sig10.txt"
+  done
+done
