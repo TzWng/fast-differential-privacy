2fc26dbfaba3a8d714fd13597181304bff765141
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 04:44:31 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -17,7 +17,7 @@ LRS=(-3 -2.5 -2 -1.5 -1 -0.5)
 # LRS=(-12 -11.5 -11 -10.5 -10)
 # LRS=(-5.5 -5 -4.5 -4 -3.5 -3 -2.5)
 
-for s in 2 3 4 5; do
+for s in 1 2 3 4 5; do
   for lr in "${LRS[@]}"; do
     $PYTHON -m scripts.vit_unifed \
       --lr "$lr" \
@@ -25,7 +25,7 @@ for s in 2 3 4 5; do
       --bs 200 \
       --mini_bs 200 \
       --epsilon 2 \
-      --noise 2 \
+      --noise 1 \
       --scale "$s" \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
