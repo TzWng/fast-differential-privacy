963aeb6194053da3fe91f2cc6ad3818ece4c4de1
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 16:47:20 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,9 +5,34 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-8.5 -8 -7.5 -7 -6.5) # SGD
+LRS=(-8 -8.5 -6) # SGD
 
-for wid in 512 800 1152 1568 2048; do
+for wid in 512; do
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print sqrt(128.0/$wid)}")
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_unifed \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension "$dim" \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_lastlayer10_ratio.txt"
+  done
+done
+
+
+LRS=(-8 -7.5 -7 -6.5 -6) # SGD
+
+for wid in 800 1152 1568 2048; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
