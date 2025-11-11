1eb2207084b6c60fa96f94cb7684676a4affafb3
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 05:27:54 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -6,9 +6,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 LRS=(-13 -12.5 -12 -11.5 -11)
 # LRS=(-13)
-LRS=(-16 -15.5)
+LRS=(-16 -15.5 -15 -14.5)
 
-for wid in 1024 2048 4096; do
+for wid in 2048 4096; do
   for lr in "${LRS[@]}"; do
     $PYTHON -m scripts.MLP_unifed \
       --width "$wid" \
