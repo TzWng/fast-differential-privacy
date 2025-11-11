08d710b217154e4c13fed0eced3b9f31db61b690
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 05:46:11 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -24,6 +24,6 @@ for wid in 256 512 1024 2048 4096 8192; do
       --cifar_data CIFAR10 \
       --dimension 32 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_sig10.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_sig10.txt"
   done
 done
