92d70668978ee5156ba77d63d103104fb3a1ebaf
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 05:38:20 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -7,8 +7,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 LRS=(-13 -12.5 -12 -11.5 -11)
 # LRS=(-13)
 LRS=(-16.5 -16 -15.5)
+LRS=(-4 -3.5 -3 -2.5 -2)
 
-for wid in 4096 8192; do
+for wid in 256 512 1024 2048 4096 8192; do
   for lr in "${LRS[@]}"; do
     $PYTHON -m scripts.MLP_unifed \
       --width "$wid" \
@@ -22,7 +23,7 @@ for wid in 4096 8192; do
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_sig10.txt"
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_sig10.txt"
   done
 done
