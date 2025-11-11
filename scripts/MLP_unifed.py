6cdcd77c1be4f2f1baa3d30244d05c909af4eddd
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 07:46:40 -0500

    Update MLP_unifed.py

--- scripts/MLP_unifed.py
+++ scripts/MLP_unifed.py
@@ -185,18 +185,18 @@ def main(args):
                    
                     if grad is not None and grad.ndim in (1, 2):
                         spec = torch.linalg.norm(grad, ord=2).clamp(min=eps)
-                        print("spectral norm is", spec)
-                        spec = (param.shape[0]**0.5 + param.shape[1]**0.5) * args.noise
-                        print("Approximate norm is", spec)
+                        # print("spectral norm is", spec)
+                        # spec = (param.shape[0]**0.5 + param.shape[1]**0.5) * args.noise
+                        # print("Approximate norm is", spec)
                         if grad.ndim == 2:
                             if args.optimizer == 'SGD':
-                                lr_scale = (param.shape[0] / param.shape[1]) ** 0.5
+                                lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / spec
                             elif args.optimizer == 'Adam':
                                 lr_scale = (param.shape[0] / param.shape[1]) ** 0.5   
                                 # lr_scale = 1 / param.shape[1]  
                         elif grad.ndim == 1:
                             if args.optimizer == 'SGD':
-                                lr_scale = (param.shape[0]) ** 0.5 # / spec
+                                lr_scale = (param.shape[0]) ** 0.5 / spec
                             elif args.optimizer == 'Adam':
                                 lr_scale = 1
                             
