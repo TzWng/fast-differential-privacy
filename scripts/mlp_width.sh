8651559c47392f09fcdba1393d9f534240f760bd
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 09:42:23 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -7,29 +7,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 LRS=(-13 -12.5 -12 -11.5 -11)
 # LRS=(-13)
 LRS=(-16.5 -16 -15.5)
-LRS=(-6 -5)
-
-for wid in 512 1024; do
-  for lr in "${LRS[@]}"; do
-    $PYTHON -m scripts.MLP_unifed \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 20\
-      --bs 2000 \
-      --mini_bs 2000 \
-      --epsilon 2 \
-      --noise 1 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/mlp_true.txt"
-  done
-done
 
 LRS=(-6 -5 -4 -3)
-for wid in 2048 4096; do
+for wid in 256; do
   for lr in "${LRS[@]}"; do
     $PYTHON -m scripts.MLP_unifed \
       --width "$wid" \
@@ -38,12 +18,12 @@ for wid in 2048 4096; do
       --bs 2000 \
       --mini_bs 2000 \
       --epsilon 2 \
-      --noise 1 \
+      --noise 0 \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension 32 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/mlp_true.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/temp.txt"
   done
 done
