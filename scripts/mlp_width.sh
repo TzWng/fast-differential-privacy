8385324c39742168ab497c6a0dd4662a22cec431
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 07:54:40 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -7,9 +7,29 @@ export PYTHONPATH="$PROJECT_ROOT"
 LRS=(-13 -12.5 -12 -11.5 -11)
 # LRS=(-13)
 LRS=(-16.5 -16 -15.5)
-LRS=(-7 -6 -5 -4)
+LRS=(-3 -2 -1 0)
 
-for wid in 256 512 1024 2048 4096; do
+for wid in 256; do
+  for lr in "${LRS[@]}"; do
+    $PYTHON -m scripts.MLP_unifed \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20\
+      --bs 2000 \
+      --mini_bs 2000 \
+      --epsilon 2 \
+      --noise 1 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/mlp_true.txt"
+  done
+done
+
+LRS=(-4 -3 -2 -1)
+for wid in 512 1024 2048 4096; do
   for lr in "${LRS[@]}"; do
     $PYTHON -m scripts.MLP_unifed \
       --width "$wid" \
