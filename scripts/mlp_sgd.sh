ebc21711a5326887bac2ecb53eb604761cf9b384
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 18:54:34 -0500

    Update mlp_sgd.sh

--- scripts/mlp_sgd.sh
+++ scripts/mlp_sgd.sh
@@ -4,7 +4,7 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-8 -7.5 -7 -6.5 -6) # SGD
+LRS=(-5.5 -5 -4.5 -4) # SGD
 
 for wid in 128; do
   for lr in "${LRS[@]}"; do
