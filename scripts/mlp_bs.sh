b35651bbe2f2d9978686b4495732fb349fa6cec9
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 06:45:25 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -6,13 +6,13 @@ BS=1024  # 你要的 batch size
 LRS=(-12.5 -12 -11.5 -11 -10.5 -10 -9.5 -9 -8.5 -7.5)
 LRS=(-9.8) # 500
 LRS=(-6 -11 -11.5 -12) #2000
-LRS=(-15 -14.5)
+LRS=(-9 -8.5 -8 -7.5)
 
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-for BS in 125; do
+for BS in 125 250 500 1000 2000; do
   epoch=$(( 5 * BS / 125 ))
   # ratio=$(echo "scale=6; x = $BS/125; l(x)/l(2)" | bc -l)
   for lr in "${LRS[@]}"; do
@@ -29,8 +29,8 @@ for BS in 125; do
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_sig10.txt"
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_sig10.txt"
   done
 done
 
