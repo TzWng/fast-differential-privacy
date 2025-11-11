3c43eb6b248dc06f0f1eec15a211a5fb0f26d1ad
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 18:30:50 -0500

    Update mlp_sgd.sh

--- scripts/mlp_sgd.sh
+++ scripts/mlp_sgd.sh
@@ -4,9 +4,9 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-7 -6.5 -6 -5.5 -5 -4.5 -4) # SGD
+LRS=(-6.5 -6 -5.5) # SGD
 
-for wid in 128; do
+for wid in 2048; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print sqrt(128.0/$wid)*16}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
