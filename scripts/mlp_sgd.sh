7acfa0b1148d9cb5888bbb6b4c9aa4391487c6b7
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 15:50:37 -0500

    Update mlp_sgd.sh

--- scripts/mlp_sgd.sh
+++ scripts/mlp_sgd.sh
@@ -4,9 +4,9 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-5 -4.5 -4) # SGD
+LRS=(-6.5 -6 -5.5 -5 -4.5) # SGD
 
-for wid in 512 1024 2048 4096; do
+for wid in 256 512 1024 2048 4096; do
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr"
     $PYTHON -m scripts.MLP_unifed \
@@ -22,6 +22,6 @@ for wid in 512 1024 2048 4096; do
       --cifar_data CIFAR10 \
       --dimension 32 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_sig10_1.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_lastlayer10_sig10.txt"
   done
 done
