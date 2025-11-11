156cb0fbd2665583e70d2b2ff0330f6a8a3945d8
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 11:57:32 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,31 +4,12 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-5.5 -7.5) # SGD
-
-for wid in 256; do
-  for lr in "${LRS[@]}"; do
-    $PYTHON -m scripts.MLP_unifed \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 20\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 1 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_sig10.txt"
-  done
-done
 
 LRS=(-7.5 -7 -6.5 -6 -5.5) # SGD
 
-for wid in 512 1024 2048 4096; do
+for wid in 256 512 1024 2048 4096; do
   for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 256.0/$wid}")
     $PYTHON -m scripts.MLP_unifed \
       --width "$wid" \
       --lr "$lr" \
@@ -36,7 +17,7 @@ for wid in 512 1024 2048 4096; do
       --bs 500 \
       --mini_bs 500 \
       --epsilon 2 \
-      --noise 1 \
+      --noise "$sig" \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
