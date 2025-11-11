528dee1c921a5200c30fc3a92e0fc2c6b29104bd
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 14:38:37 -0500

    Update MLP_unifed.py

--- scripts/MLP_unifed.py
+++ scripts/MLP_unifed.py
@@ -179,19 +179,17 @@ def main(args):
                     grad = param.private_grad
                     lr_scale = 1.0                  
                    
-                if grad is not None and grad.ndim in (1, 2):
-                    spec = torch.linalg.norm(grad, ord=2).clamp(min=eps) / args.bs
-                    
-                    if param.shape[1] == 3 * args.dimension * args.dimension:
-                        lr_scale = (param.shape[0] / 32.0) ** 0.5 / spec
-                    elif param.shape[0] == 10:
-                        lr_scale = (1.0 / param.shape[1]) ** 0.5 / spec
-                    elif grad.ndim == 2:
-                        lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / spec             
-                    elif grad.ndim == 1:
-                        lr_scale = (param.shape[0]) ** 0.5 / spec
-
-
+                    if grad is not None and grad.ndim in (1, 2):
+                        spec = torch.linalg.norm(grad, ord=2).clamp(min=eps) / args.bs
+                        
+                        if param.shape[1] == 3 * args.dimension * args.dimension:
+                            lr_scale = (param.shape[0] / 32.0) ** 0.5 / spec
+                        elif param.shape[0] == 10:
+                            lr_scale = (1.0 / param.shape[1]) ** 0.5 / spec
+                        elif grad.ndim == 2:
+                            lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / spec             
+                        elif grad.ndim == 1:
+                            lr_scale = (param.shape[0]) ** 0.5 / spec
                             
                     group["lr"] = base_lr * lr_scale
 
