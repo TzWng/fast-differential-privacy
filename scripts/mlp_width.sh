c134718e3832283b94a77abc367f1fc6169dfbc1
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 11:17:56 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,11 +4,9 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-7 -7.5) # SGD
+LRS=(-7 -6.5 -6 -5.5 -5) # SGD
 
-LRS=(-16 -15 -14.5 -14 -13.5) # Adam
-
-for wid in 512; do
+for wid in 256 512 1024 2048 4096; do
   for lr in "${LRS[@]}"; do
     $PYTHON -m scripts.MLP_unifed \
       --width "$wid" \
@@ -22,29 +20,8 @@ for wid in 512; do
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_truenorm_sig10.txt"
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_sig10.txt"
   done
 done
 
-
-LRS=(-17 -16.5 -16 -15.5 -15 -14.5 -14 -13.5) # Adam
-
-for wid in 1024 2048 4096; do
-  for lr in "${LRS[@]}"; do
-    $PYTHON -m scripts.MLP_unifed \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 20\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 1 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_truenorm_sig10.txt"
-  done
-done
