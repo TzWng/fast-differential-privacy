c535442df924bd24134938a1510549aa4ffbb171
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 12:19:34 -0500

    Update mlp_adam.sh

--- scripts/mlp_adam.sh
+++ scripts/mlp_adam.sh
@@ -5,33 +5,9 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-14.5) # Adam
+LRS=(-16 -15.5 -15 -14.5 -14) # Adam
 
-for wid in 256; do
-  for lr in "${LRS[@]}"; do
-    sig=$(awk "BEGIN {print 256.0/$wid}")
-    $PYTHON -m scripts.MLP_unifed \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 20\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise "$sig" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_truenorm_ratio.txt"
-  done
-done
-
-
-
-LRS=(-16.5 -16 -15.5 -15 -14.5) # Adam
-
-for wid in 512 1024 2048 4096; do
+for wid in 256 512 1024 2048 4096; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 256.0/$wid}")
     $PYTHON -m scripts.MLP_unifed \
