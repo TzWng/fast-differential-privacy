81c2d128502535943440902c4922560151c65cff
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 07:10:41 -0500

    Update vit_unifed.py

--- scripts/vit_unifed.py
+++ scripts/vit_unifed.py
@@ -146,7 +146,7 @@ def main(args):
                         if grad is not None and grad.ndim in (1, 2):
                             spec = torch.linalg.norm(grad, ord=2).clamp(min=eps)
                             print("spectral norm is", spec)
-                            spec = (param.shape[0]**0.5 + param.shape[1]**0.5) * args.noise / args.bs
+                            spec_a = (param.shape[0]**0.5 + param.shape[1]**0.5) * args.noise / args.bs
                             print("Approximate norm is", spec_a)
                             if grad.ndim == 2:
                                 lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / spec
