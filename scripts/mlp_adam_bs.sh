e6978f86306f3d15498493afcd427a4e9b87d75d
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-12 13:42:12 -0500

    Create mlp_adam_bs.sh

---
+++ scripts/mlp_adam_bs.sh
@@ -0,0 +1,22 @@
+LRS=(-14 -13.5 -13 -12.5)
+
+for BS in 125 250 500 1000 2000; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    echo "Running BS=$BS, lr=$lr, epoch=$epoch" 
+    $PYTHON -m scripts.MLP_unifed \
+      --width 512 \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 4 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_truenorm.txt"
+  done
+done
