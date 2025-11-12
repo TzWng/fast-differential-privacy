dd5c2d91f4924d85c2fa283b872e345f5aae50c1
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-12 15:01:30 -0500

    Update mlp_adam.sh

--- scripts/mlp_adam.sh
+++ scripts/mlp_adam.sh
@@ -27,9 +27,33 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
+# #288 512 1152 2048 3200 4608
+# LRS=(-14 -13.5 -13 -12.5)
+# for wid in 1152; do
+#   for lr in "${LRS[@]}"; do
+#     sig=$(awk "BEGIN {print 128.0/$wid*3.0}")
+#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+#     $PYTHON -m scripts.MLP_unifed \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 20 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise "$sig" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --cifar_data CIFAR10 \
+#       --dimension "$dim" \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_truenorm_ratio_2.txt"
+#   done
+# done
+
 #288 512 1152 2048 3200 4608
-LRS=(-14 -13.5 -13 -12.5)
-for wid in 1152; do
+LRS=(-15 -14.5 -14 -13.5 -13 -12.5)
+for wid in 228; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 128.0/$wid*3.0}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
