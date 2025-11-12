0b58c3daa22e72cd7d1a0ee5e4f0adeb4a1fc584
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-12 07:59:32 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,33 +5,9 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-# LRS=(-4 -7) # SGD
+LRS=(-7) # SGD
 
-# for wid in 2048; do
-#   for lr in "${LRS[@]}"; do
-#     sig=$(awk "BEGIN {print sqrt(128.0/$wid)}")
-#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-#     $PYTHON -m scripts.MLP_unifed \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 20 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise "$sig" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --cifar_data CIFAR10 \
-#       --dimension "$dim" \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_1.txt"
-#   done
-# done
-
-LRS=(-4 -6 -6.5 -7) # SGD
-
-for wid in 3200; do
+for wid in 4608; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
@@ -52,6 +28,30 @@ for wid in 3200; do
       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_1.txt"
   done
 done
+
+# LRS=(-4 -6 -6.5 -7) # SGD
+
+# for wid in 3200; do
+#   for lr in "${LRS[@]}"; do
+#     sig=$(awk "BEGIN {print sqrt(128.0/$wid)}")
+#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+#     $PYTHON -m scripts.MLP_unifed \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 20 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise "$sig" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --cifar_data CIFAR10 \
+#       --dimension "$dim" \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_1.txt"
+#   done
+# done
 
 # LRS=(-5 -6 -6.5) # SGD
 
