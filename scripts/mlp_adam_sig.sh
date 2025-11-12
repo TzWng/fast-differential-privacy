f0b6e2fd4df01ca5fcb252564d4f64e6c25ee1f1
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-12 13:47:04 -0500

    Create mlp_adam_sig.sh

---
+++ scripts/mlp_adam_sig.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+PYTHON=python3.10
+
+PROJECT_ROOT=/content/fast-differential-privacy
+export PYTHONPATH="$PROJECT_ROOT"
+
+LRS=(-12 -11.5 -11 -10.5 -10)
+
+for sig in 2 4 8 16; do
+  for lr in "${LRS[@]}"; do
+    echo "Running BS=$BS, lr=$lr, noise=$sig" 
+    $PYTHON -m scripts.MLP_unifed \
+      --width 256 \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffsig_truenorm.txt"
+  done
+done
