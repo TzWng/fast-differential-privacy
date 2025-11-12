d3c20e061279dcffcd96546e1076ca8d16b365ef
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-11 20:02:12 -0500

    Update mlp_sgd.sh

--- scripts/mlp_sgd.sh
+++ scripts/mlp_sgd.sh
@@ -3,8 +3,31 @@ PYTHON=python3.10
 
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
+LRS=(-6 -5.5 -5 -4.5 -4 -3.5 -3) # SGD
 
-LRS=(-5.5 -5 -4.5 -4) # SGD
+for wid in 288; do
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print sqrt(128.0/$wid)*16}")
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_unifed \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension "$dim" \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_temp.txt"
+  done
+done
+
+LRS=(-4 -3.5 -3 -6) # SGD
 
 for wid in 2048; do
   for lr in "${LRS[@]}"; do
