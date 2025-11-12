f30a3e18e69df681fa772cbe5afa210a0bdc706f
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-12 12:29:47 -0500

    Update mlp_adam.sh

--- scripts/mlp_adam.sh
+++ scripts/mlp_adam.sh
@@ -29,7 +29,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 #288 512 1152 2048 3200 4608
 LRS=(-16 -15.5 -15 -14.5)
-for wid in 1152 2048 3200 4608; do
+for wid in 288 512 1152 2048 3200 4608; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print sqrt(128.0/$wid)}*2.0")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
