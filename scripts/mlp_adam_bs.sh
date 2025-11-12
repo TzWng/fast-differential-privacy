dff40ef247d548b916c6a9c946349f28325064e9
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-12 14:07:53 -0500

    Update mlp_adam_bs.sh

--- scripts/mlp_adam_bs.sh
+++ scripts/mlp_adam_bs.sh
@@ -4,9 +4,9 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-10.5 -10 -9.5 -9)
+LRS=(-11 -11.5 -12 -12..5)
 
-for BS in 125 250 500 1000 2000; do
+for BS in 125; do
   epoch=$(( 4 * BS / 125 ))
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 2*$BS/125.0}")
@@ -27,3 +27,5 @@ for BS in 125 250 500 1000 2000; do
       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_truenorm.txt"
   done
 done
+
+#250 500 1000 2000
