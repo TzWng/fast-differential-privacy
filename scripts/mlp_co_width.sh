9c65055ad68b7af5582e824571105b5e712094a5
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 13:06:48 -0500

    Update mlp_co_width.sh

--- scripts/mlp_co_width.sh
+++ scripts/mlp_co_width.sh
@@ -5,10 +5,32 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-2.5 -2 -1.5 -1 -0.5) # SGD
-# LRS=(-6.5) # SGD
+# LRS=(-2.5 -2 -1.5 -1 -0.5) # SGD
+LRS=(-0.5) # SGD
 
-for wid in 256 512 1024 2048 4096; do
+for wid in 512 1024 2048 4096; do
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwid_approx.txt"
+  done
+done
+
+LRS=(0 0.5 1) # SGD
+
+for wid in 1024 2048 4096; do
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr"
     $PYTHON -m scripts.MLP_clipping_only \
