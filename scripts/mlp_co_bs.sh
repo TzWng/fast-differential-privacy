b52c76e93cd67be64f8e00f3d0702927cdfda663
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 10:09:07 -0500

    Create mlp_co_bs.sh

---
+++ scripts/mlp_co_bs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+PYTHON=python3.10
+
+PROJECT_ROOT=/content/fast-differential-privacy
+export PYTHONPATH="$PROJECT_ROOT"
+
+
+LRS=(-8 -7.5 -7 -6.5 -5 -5.5 -5 -4.5 -4 -3.5) # SGD
+
+for BS in 125; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, bs=$BS"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs "$epoch" \
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_co.txt"
+  done
+done
