c61dffdd80d694d192fd1d51d21ac937a018a437
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 13:58:48 -0500

    Update MLP_unifed.py

--- scripts/MLP_unifed.py
+++ scripts/MLP_unifed.py
@@ -185,7 +185,7 @@ def main(args):
                         if param.shape[1] == 3 * args.dimension * args.dimension:
                             lr_scale = (param.shape[0]) ** 0.5 / args.dimension / spec
                         elif param.shape[0] == 10:
-                            lr_scale = (1.0 / param.shape[1]) ** 0.5 / spec
+                            lr_scale = (10 / param.shape[1]) ** 0.5 / spec
                         elif grad.ndim == 2:
                             lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / spec             
                         elif grad.ndim == 1:
