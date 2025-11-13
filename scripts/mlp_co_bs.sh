99089734c956a8d7efb6ec0d9d7b86fcb391fac3
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 13:36:45 -0500

    Update mlp_co_bs.sh

--- scripts/mlp_co_bs.sh
+++ scripts/mlp_co_bs.sh
@@ -104,9 +104,31 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-LRS=(-3 -2.5 -2 -1.5 -1 -0.5 0) # SGD
+LRS=(-3.5 -4 ) # SGD
 
-for wid in 256 512 1024 2048 4096; do
+for wid in 256; do
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwid_approx_10.txt"
+  done
+done
+
+LRS=(-4 -3.5 -3 -2.5 -2) # SGD
+
+for wid in 512 1024 2048 4096; do
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr"
     $PYTHON -m scripts.MLP_clipping_only \
