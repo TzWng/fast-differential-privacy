5b3116a7da4596576b6fa353ac504f04c5b64d48
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 10:19:03 -0500

    Update mlp_co_bs.sh

--- scripts/mlp_co_bs.sh
+++ scripts/mlp_co_bs.sh
@@ -5,7 +5,7 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-5 -4.5 -4 -3.5 -3 -2.5 -2) # SGD
+LRS=(-1.5 -1 -0.5 0 0.5) # SGD
 
 for BS in 125; do
   epoch=$(( 4 * BS / 125 ))
