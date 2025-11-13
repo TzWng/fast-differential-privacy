caf8a090144e37a923137ff1ccc81dd0909a3ce9
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 12:08:31 -0500

    Update mlp_co_width.sh

--- scripts/mlp_co_width.sh
+++ scripts/mlp_co_width.sh
@@ -28,9 +28,32 @@ LRS=(-3 -2.5 -2 -1.5 -1) # SGD
 #   done
 # done
 
-LRS=(-7 -5.5) # SGD
+LRS=(-6 -5.5) # SGD
 
-for wid in 512; do
+for wid in 256; do
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwid_truenorm.txt"
+  done
+done
+
+
+LRS=(-6 -5.5 -5) # SGD
+
+for wid in 1024; do
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr"
     $PYTHON -m scripts.MLP_clipping_only \
