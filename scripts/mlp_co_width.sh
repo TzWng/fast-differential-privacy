5229c3956fc0859d3f61abbc24913f38600b1382
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 18:10:20 -0500

    Update mlp_co_width.sh

--- scripts/mlp_co_width.sh
+++ scripts/mlp_co_width.sh
@@ -5,43 +5,18 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-1.5 -2 -2.5 -3) # SGD
+# LRS=(-1.5 -2 -2.5 -3) # SGD
 
-for BS in 125 250 500 1000; do
-  epoch=$(( 4 * BS / 125 ))
-  for lr in "${LRS[@]}"; do
-    echo "Running bs=$BS, epoch=$epoch, lr=$lr"
-    $PYTHON -m scripts.MLP_clipping_only \
-      --width 512 \
-      --lr "$lr" \
-      --epochs "$epoch" \
-      --bs "$BS" \
-      --mini_bs "$BS" \
-      --epsilon 2 \
-      --noise 0 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_approx_10.txt"
-  done
-done
-
-
-
-# # LRS=(-2.5 -2 -1.5 -1 -0.5) # SGD
-# LRS=(-2.5 -2 -3) # SGD
-
-# for wid in 1024 2048; do
+# for BS in 125 250 500 1000; do
+#   epoch=$(( 4 * BS / 125 ))
 #   for lr in "${LRS[@]}"; do
-#     echo "Running width=$wid, lr=$lr"
+#     echo "Running bs=$BS, epoch=$epoch, lr=$lr"
 #     $PYTHON -m scripts.MLP_clipping_only \
-#       --width "$wid" \
+#       --width 512 \
 #       --lr "$lr" \
-#       --epochs 20 \
-#       --bs 500 \
-#       --mini_bs 500 \
+#       --epochs "$epoch" \
+#       --bs "$BS" \
+#       --mini_bs "$BS" \
 #       --epsilon 2 \
 #       --noise 0 \
 #       --clipping_mode BK-MixOpt \
@@ -49,9 +24,34 @@ done
 #       --cifar_data CIFAR10 \
 #       --dimension 32 \
 #       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwid_approx.txt"
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_approx_10.txt"
 #   done
 # done
+
+
+
+# LRS=(-2.5 -2 -1.5 -1 -0.5) # SGD
+LRS=(-3.5 -3 -2.5 -2 -1.5) # Adam
+
+for wid in 256; do
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwid_approx.txt"
+  done
+done
 
 # LRS=(-1 -1.5 -2 -2.5 -3) # SGD
 
