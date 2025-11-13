62e49c1c5a0467b9ac2b53d4bd03ca07153bd766
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 10:46:34 -0500

    Update mlp_co_bs.sh

--- scripts/mlp_co_bs.sh
+++ scripts/mlp_co_bs.sh
@@ -5,9 +5,32 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-2.5 -2 -1.5 -1 -0.5) # SGD
+LRS=(-3) # SGD
 
 for BS in 125; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    echo "Running bs=$BS, epoch=$epoch, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width 1024 \
+      --lr "$lr" \
+      --epochs "$epoch" \
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_co.txt"
+  done
+done
+
+LRS=(-3 -2.5 -2 -1.5) # SGD
+
+for BS in 1000; do
   epoch=$(( 4 * BS / 125 ))
   for lr in "${LRS[@]}"; do
     echo "Running bs=$BS, epoch=$epoch, lr=$lr"
