e8fb378e96ef91480abdec6b6836493f2d04e82d
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 15:17:48 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -29,9 +29,34 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-7.5 -8) # SGD
+# LRS=(-7.5 -8) # SGD
 
-for wid in 288 512; do
+# for wid in 288 512; do
+#   for lr in "${LRS[@]}"; do
+#     sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
+#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+#     $PYTHON -m scripts.MLP_unifed \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 20 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise "$sig" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --cifar_data CIFAR10 \
+#       --dimension "$dim" \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_1.txt"
+#   done
+# done
+
+
+LRS=(-7.5 -7) # SGD
+
+for wid in 2048; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
@@ -49,32 +74,7 @@ for wid in 288 512; do
       --cifar_data CIFAR10 \
       --dimension "$dim" \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_1.txt"
-  done
-done
-
-
-LRS=(-6 -5.5) # SGD
-
-for wid in 512; do
-  for lr in "${LRS[@]}"; do
-    sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
-    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-    $PYTHON -m scripts.MLP_unifed \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 20 \
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise "$sig" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension "$dim" \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_1.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_2.txt"
   done
 done
 
