692aa7c7290b713a116ee279e3994004fd66aa91
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 14:28:13 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -31,7 +31,32 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 LRS=(-6.5 -7) # SGD
 
-for wid in 288 1152; do
+for wid in 288 512; do
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_unifed \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension "$dim" \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio.txt"
+  done
+done
+
+
+LRS=(-6 -5.5) # SGD
+
+for wid in 512; do
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
