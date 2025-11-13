28dd663e8a913ae3b79e305bf5222072bc22e69e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 17:48:45 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -74,7 +74,7 @@ for wid in 2048; do
       --cifar_data CIFAR10 \
       --dimension "$dim" \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_truenorm_ratio_2.txt"
   done
 done
 
