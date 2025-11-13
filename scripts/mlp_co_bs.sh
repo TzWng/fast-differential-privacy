72203f39e2eddea102387433fc414861343042ab
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 18:12:08 -0500

    Update mlp_co_bs.sh

--- scripts/mlp_co_bs.sh
+++ scripts/mlp_co_bs.sh
@@ -79,49 +79,49 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-# LRS=(-3 -2.5 -2) # SGD
+LRS=(-3.5 -3 -2.5 -2 -1.5) # SGD
 
-# for BS in 1000; do
-#   epoch=$(( 4 * BS / 125 ))
-#   for lr in "${LRS[@]}"; do
-#     echo "Running bs=$BS, epoch=$epoch, lr=$lr"
-#     $PYTHON -m scripts.MLP_clipping_only \
-#       --width 512 \
-#       --lr "$lr" \
-#       --epochs "$epoch" \
-#       --bs "$BS" \
-#       --mini_bs "$BS" \
-#       --epsilon 2 \
-#       --noise 0 \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --cifar_data CIFAR10 \
-#       --dimension 32 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_approx.txt"
-#   done
-# done
-
-
-
-LRS=(-1.5 -1 -0.5) # SGD
-
-for lr in "${LRS[@]}"; do
-  for wid in 512 1024 2048 4096; do
-    echo "Running width=$wid, lr=$lr"
+for BS in 125; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    echo "Running bs=$BS, epoch=$epoch, lr=$lr"
     $PYTHON -m scripts.MLP_clipping_only \
-      --width "$wid" \
+      --width 512 \
       --lr "$lr" \
-      --epochs 20 \
-      --bs 500 \
-      --mini_bs 500 \
+      --epochs "$epoch" \
+      --bs "$BS" \
+      --mini_bs "$BS" \
       --epsilon 2 \
       --noise 0 \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwid_approx_10.txt"
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_approx.txt"
   done
 done
+
+
+
+# LRS=(-1.5 -1 -0.5) # SGD
+
+# for lr in "${LRS[@]}"; do
+#   for wid in 256; do
+#     echo "Running width=$wid, lr=$lr"
+#     $PYTHON -m scripts.MLP_clipping_only \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 20 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 0 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --cifar_data CIFAR10 \
+#       --dimension 32 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwid_approx.txt"
+#   done
+# done
