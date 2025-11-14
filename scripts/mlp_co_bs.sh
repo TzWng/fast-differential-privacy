0973f6603ac0d0f1ce5fe5cf029b4b383dd6eb05
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 22:30:47 -0500

    Update mlp_co_bs.sh

--- scripts/mlp_co_bs.sh
+++ scripts/mlp_co_bs.sh
@@ -28,6 +28,30 @@ for BS in 250 500 1000; do
   done
 done
 
+
+LRS=(-11 -9 -8 -7 -6 -5 -4) # SGD
+
+for BS in 500 1000; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    echo "Running bs=$BS, epoch=$epoch, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width 2048 \
+      --lr "$lr" \
+      --epochs "$epoch" \
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_approx_10.txt"
+  done
+done
+
 # LRS=(-1 -0.5 0) # SGD
 
 # for BS in 125; do
