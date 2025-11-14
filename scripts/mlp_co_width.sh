d01cf64d4dd308fb43c9e8859c5f2616fa0a5559
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-14 00:40:21 -0500

    Update mlp_co_width.sh

--- scripts/mlp_co_width.sh
+++ scripts/mlp_co_width.sh
@@ -6,9 +6,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 LRS=(-3 -2.5 -2 -1.5 -1) # Adam
-LRS=(1 2 3) # Adam
+LRS=(-2 -1 0 1 2) # Adam
 
-for wid in 512; do
+for wid in 256 1024 2048 4096 8192; do
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr"
     $PYTHON -m scripts.MLP_clipping_only \
