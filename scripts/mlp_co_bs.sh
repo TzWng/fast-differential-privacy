eda7d047e7e0d6f48fde6e154b7ca53040af040c
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 23:27:46 -0500

    Update mlp_co_bs.sh

--- scripts/mlp_co_bs.sh
+++ scripts/mlp_co_bs.sh
@@ -5,14 +5,14 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-8 -7 -6 -5 -4 -3) # SGD
+LRS=(-3.5 -4 -4.5) # SGD
 
-for BS in 2000; do
+for BS in 125 250; do
   epoch=$(( 4 * BS / 125 ))
   for lr in "${LRS[@]}"; do
     echo "Running bs=$BS, epoch=$epoch, lr=$lr"
     $PYTHON -m scripts.MLP_clipping_only \
-      --width 2048 \
+      --width 512 \
       --lr "$lr" \
       --epochs "$epoch" \
       --bs "$BS" \
@@ -23,10 +23,36 @@ for BS in 2000; do
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_approx_10.txt"
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_approx_10.txt"
   done
 done
+
+
+LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 -4) # SGD
+
+for BS in 500 1000 2000; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    echo "Running bs=$BS, epoch=$epoch, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width 512 \
+      --lr "$lr" \
+      --epochs "$epoch" \
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_approx_10.txt"
+  done
+done
+
+
 
 # LRS=(-1 -0.5 0) # SGD
 
