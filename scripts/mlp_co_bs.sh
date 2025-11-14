f109c039db2a95cd582c848aa0205a5f22ffde2e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-14 00:10:24 -0500

    Update mlp_co_bs.sh

--- scripts/mlp_co_bs.sh
+++ scripts/mlp_co_bs.sh
@@ -4,9 +4,9 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(0 1 2 3) # SGD
+LRS=(0 1 2) # SGD
 
-for BS in 500; do
+for BS in 250; do
   epoch=$(( 4 * BS / 125 ))
   for lr in "${LRS[@]}"; do
     echo "Running bs=$BS, epoch=$epoch, lr=$lr"
@@ -27,9 +27,32 @@ for BS in 500; do
   done
 done
 
-LRS=(-1 0 1 2 3) # SGD
+LRS=(-1 0 1 2 -2 -3 -4) # SGD
 
-for BS in 1000 2000; do
+for BS in 2000; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    echo "Running bs=$BS, epoch=$epoch, lr=$lr"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width 512 \
+      --lr "$lr" \
+      --epochs "$epoch" \
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/temp.txt"
+  done
+done
+
+LRS=(-2 -3 -4) # SGD
+
+for BS in 1000; do
   epoch=$(( 4 * BS / 125 ))
   for lr in "${LRS[@]}"; do
     echo "Running bs=$BS, epoch=$epoch, lr=$lr"
