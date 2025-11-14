2eb1a8f9cc804917d64d9d992269e36567dc5b44
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-13 22:24:07 -0500

    Update mlp_co_width.sh

--- scripts/mlp_co_width.sh
+++ scripts/mlp_co_width.sh
@@ -28,9 +28,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-1.5 -1) # SGD
+LRS=(-7 -6 -5 -4 -3) # SGD
 
-for wid in 512; do
+for wid in 256; do
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr"
     $PYTHON -m scripts.MLP_clipping_only \
@@ -45,33 +45,11 @@ for wid in 512; do
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwid_approx.txt"
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwid_approx.txt"
   done
 done
 
-
-LRS=(-3 -2.5 -2 -1.5 -1) # SGD
-
-for wid in 1024 2048 4096; do
-  for lr in "${LRS[@]}"; do
-    echo "Running width=$wid, lr=$lr"
-    $PYTHON -m scripts.MLP_clipping_only \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 20 \
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 0 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwid_approx.txt"
-  done
-done
 
 
 # LRS=(-6 -5.5 -5) # SGD
