56f7294f24d83d6b4315f5f13b0c3081cda85575
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-14 17:01:31 -0500

    Update MLP_unifed.py

--- scripts/MLP_unifed.py
+++ scripts/MLP_unifed.py
@@ -207,8 +207,6 @@ def main(args):
                         spec = torch.linalg.norm(grad, ord=2).clamp(min=eps) / args.bs                    
                         if param.shape[1] == 3 * args.dimension * args.dimension:
                             lr_scale = (param.shape[0]) ** 0.5 / args.dimension / spec
-                        # elif param.shape[0] == 10:
-                        #     lr_scale = (1.0 / param.shape[1]) ** 0.5 / spec
                         elif grad.ndim == 2:
                             lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / spec             
                         elif grad.ndim == 1:
