90d0dd0e447819840ae3adfa30aaaf7461f0c43d
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-15 11:46:32 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -27,10 +27,35 @@ BS=1024  # 你要的 batch size
 #   done
 # done
 
+LRS=(-7)
 
-LRS=(-12 -11 -10 -9 -8)
+for BS in 125; do
+  epoch=$(( 4 * BS / 125 ))
+  # ratio=$(echo "scale=6; x = $BS/125; l(x)/l(2)" | bc -l)
+  for lr in "${LRS[@]}"; do
+    echo "Running BS=$BS, lr=$lr" 
+    # scaled_lr=$(echo "$lr + $ratio" | bc -l)
+    $PYTHON -m scripts.MLP_unifed \
+      --width 1024 \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise 4 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_fnorm_ratio.txt"
+  done
+done
 
-for BS in 125 250 500 1000 2000; do
+
+LRS=(-11 -10 -9 -8 -7)
+
+for BS in 250 500 1000 2000; do
   epoch=$(( 4 * BS / 125 ))
   # ratio=$(echo "scale=6; x = $BS/125; l(x)/l(2)" | bc -l)
   for lr in "${LRS[@]}"; do
