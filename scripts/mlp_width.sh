cd4ecfbb477ba72e3378230b4850f147c575efe3
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-15 15:01:29 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,29 +4,29 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-# LRS=(-2) # SGD
-# # 288 512 1152 2048 3200 4608
-# for wid in 288 512 1152 2048 3200; do
-#   for lr in "${LRS[@]}"; do
-#     sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
-#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-#     $PYTHON -m scripts.MLP_unifed \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 10 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise "$sig" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --cifar_data CIFAR10 \
-#       --dimension "$dim" \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_fnorm_ratio.txt"
-#   done
-# done
+LRS=(-13 -12 -11 -10) # SGD
+# 288 512 1152 2048 3200 4608
+for wid in 288; do
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_unifed \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension "$dim" \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_truenorm_ratio.txt"
+  done
+done
 
 # LRS=(-7 -6 -5 -4 -3 -2) # SGD
 # # 288 512 1152 2048 3200 4608
@@ -53,26 +53,26 @@ export PYTHONPATH="$PROJECT_ROOT"
 # done
 
 
-LRS=(-15 -10) # SGD
-# 288 512 1152 2048 3200 4608
-for wid in 512 1152 2048 3200 4608; do
-  for lr in "${LRS[@]}"; do
-    sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
-    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-    $PYTHON -m scripts.MLP_unifed \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise "$sig" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension "$dim" \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_fnorm_ratio.txt"
-  done
-done
+# LRS=(-15 -10) # SGD
+# # 288 512 1152 2048 3200 4608
+# for wid in 512 1152 2048 3200 4608; do
+#   for lr in "${LRS[@]}"; do
+#     sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
+#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+#     $PYTHON -m scripts.MLP_unifed \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 10 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise "$sig" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --cifar_data CIFAR10 \
+#       --dimension "$dim" \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_fnorm_ratio.txt"
+#   done
+# done
