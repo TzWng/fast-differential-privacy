4648475f9e4ee19226cadd5a9311664ffdd763a9
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-15 14:19:20 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -3,34 +3,59 @@
 PYTHON=python3.10
 BS=1024  # 你要的 batch size
 
-# LRS=(-7 -6 -5 -4 -3)
-# for BS in 250 500 1000 2000; do
-#   epoch=$(( 4 * BS / 125 ))
-#   for lr in "${LRS[@]}"; do
-#     sig=$(awk "BEGIN {print 4*$BS/125.0}")
-#     echo "Running BS=$BS, lr=$lr, noise=$sig" 
-#     # scaled_lr=$(echo "$lr + $ratio" | bc -l)
-#     $PYTHON -m scripts.MLP_unifed \
-#       --width 256 \
-#       --lr "$lr" \
-#       --epochs "$epoch"\
-#       --bs "$BS" \
-#       --mini_bs "$BS" \
-#       --epsilon 2 \
-#       --noise "$sig" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --cifar_data CIFAR10 \
-#       --dimension 32 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_fnorm_ratio.txt"
-#   done
-# done
+LRS=(-8)
+for BS in 250 500 1000 2000; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 4*$BS/125.0}")
+    echo "Running BS=$BS, lr=$lr, noise=$sig" 
+    # scaled_lr=$(echo "$lr + $ratio" | bc -l)
+    $PYTHON -m scripts.MLP_unifed \
+      --width 256 \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffbs_fnorm_ratio.txt"
+  done
+done
+
+
+LRS=(-13)
+for BS in 250 500 1000 2000; do
+  epoch=$(( 4 * BS / 125 ))
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 4*$BS/125.0}")
+    echo "Running BS=$BS, lr=$lr, noise=$sig" 
+    # scaled_lr=$(echo "$lr + $ratio" | bc -l)
+    $PYTHON -m scripts.MLP_unifed \
+      --width 256 \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_fnorm_ratio.txt"
+  done
+done
 
 
 
 
-LRS=(-11 -10 -9 -8 -7)
+LRS=(-12 -13)
 for BS in 250 500 1000 2000; do
   epoch=$(( 4 * BS / 125 ))
   for lr in "${LRS[@]}"; do
