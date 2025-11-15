3dc0ae3b4c5fbf7ca9ba94be0b375f4213c10471
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-15 11:57:49 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -27,22 +27,24 @@ BS=1024  # 你要的 batch size
 #   done
 # done
 
-LRS=(-7)
 
-for BS in 125; do
+
+
+LRS=(-12 -11 -10 -9 -8 -7)
+for BS in 125 250 500 1000 2000; do
   epoch=$(( 4 * BS / 125 ))
-  # ratio=$(echo "scale=6; x = $BS/125; l(x)/l(2)" | bc -l)
   for lr in "${LRS[@]}"; do
-    echo "Running BS=$BS, lr=$lr" 
+    sig=$(awk "BEGIN {print 4*$BS/125.0}")
+    echo "Running BS=$BS, lr=$lr, noise=$sig" 
     # scaled_lr=$(echo "$lr + $ratio" | bc -l)
     $PYTHON -m scripts.MLP_unifed \
-      --width 1024 \
+      --width 256 \
       --lr "$lr" \
       --epochs "$epoch"\
       --bs "$BS" \
       --mini_bs "$BS" \
       --epsilon 2 \
-      --noise 4 \
+      --noise "$sig" \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
@@ -53,28 +55,4 @@ for BS in 125; do
 done
 
 
-LRS=(-11 -10 -9 -8 -7)
-
-for BS in 250 500 1000 2000; do
-  epoch=$(( 4 * BS / 125 ))
-  # ratio=$(echo "scale=6; x = $BS/125; l(x)/l(2)" | bc -l)
-  for lr in "${LRS[@]}"; do
-    echo "Running BS=$BS, lr=$lr" 
-    # scaled_lr=$(echo "$lr + $ratio" | bc -l)
-    $PYTHON -m scripts.MLP_unifed \
-      --width 1024 \
-      --lr "$lr" \
-      --epochs "$epoch"\
-      --bs "$BS" \
-      --mini_bs "$BS" \
-      --epsilon 2 \
-      --noise 4 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffbs_fnorm_ratio.txt"
-  done
-done
 
