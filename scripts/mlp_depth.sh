1b5465168418590490408a3e53bfe788a149906b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-15 20:17:55 -0500

    Adjust noise calculation and batch sizes in script

--- scripts/mlp_depth.sh
+++ scripts/mlp_depth.sh
@@ -10,15 +10,15 @@ LRS=(-7.5 -7 -6.5 -6 -5.5)
 
 for depth in 3 6 12 24; do
   for lr in "${LRS[@]}"; do
-    sig=$(awk "BEGIN {print sqrt($depth/3)*16.0}")
+    sig=$(awk "BEGIN {print sqrt($depth/3)*4.0}")
     echo "Running width=256, depth=$depth, lr=$lr, noise=$sig"
     $PYTHON -m scripts.MLP_unifed \
       --width 256 \
       --layer "$depth"\
       --lr "$lr" \
       --epochs 16 \
-      --bs 500 \
-      --mini_bs 500 \
+      --bs 125 \
+      --mini_bs 125 \
       --epsilon 2 \
       --noise "$sig" \
       --clipping_mode BK-MixOpt \
