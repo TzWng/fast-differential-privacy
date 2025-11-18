6766d23e87a42b9204e5564b929e1bdd093f176e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-18 18:45:10 -0500

    Update learning rates and model execution script

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,38 +4,14 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-# LRS=(-12 -13 -14 -15 -16 -17) # SGD
-# # 288 512 1152 2048 3200 4608
-# for wid in 512 1152 2048 3200 4608; do
-#   for lr in "${LRS[@]}"; do
-#     sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
-#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-#     $PYTHON -m scripts.MLP_unifed \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 10 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise "$sig" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --cifar_data CIFAR10 \
-#       --dimension "$dim" \
-#       --optimizer Adam \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_diffwidth_truenorm_ratio.txt"
-#   done
-# done
-
-LRS=(-4.5) # SGD
+LRS=(-8 -7 -6 -5 -4) # SGD
 # 288 512 1152 2048 3200 4608
-for lr in "${LRS[@]}"; do
-  for wid in 2048 1152 512 288; do
-    sig=$(awk "BEGIN {print 4.0*sqrt(128.0/$wid)}")
+for wid in 288; do
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-    $PYTHON -m scripts.MLP_unifed \
+    $PYTHON -m scripts.MLP_approx \
       --width "$wid" \
       --lr "$lr" \
       --epochs 10 \
@@ -48,11 +24,9 @@ for lr in "${LRS[@]}"; do
       --cifar_data CIFAR10 \
       --dimension "$dim" \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_truenorm_ratio_1.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_approx_ratio.txt"
   done
 done
-
-
 
 
 # LRS=(-15 -10) # SGD
