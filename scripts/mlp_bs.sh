e577369dbcc88e9e0713f9985e87430b831770b3
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-18 22:37:54 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -54,8 +54,32 @@ BS=1024  # 你要的 batch size
 #   done
 # done
 
+LRS=(-4.5)
+# 125 250 500 1000 2000
+for lr in "${LRS[@]}"; do
+  for BS in 125; do
+    epoch=$(( 4 * BS / 125 ))
+    sig=$(awk "BEGIN {print 1*$BS/125.0}")
+    echo "Running BS=$BS, lr=$lr, noise=$sig" 
+    # scaled_lr=$(echo "$lr + $ratio" | bc -l)
+    $PYTHON -m scripts.MLP_approx \
+      --width 1024 \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_depth5_diffbs_approx_ratio_1024.txt"
+  done
+done
 
-LRS=(-5 -6 -7 -8 -9)
+LRS=(-4.5 -5 -6 -7 -8 -9)
 # 125 250 500 1000 2000
 for lr in "${LRS[@]}"; do
   for BS in 250 500 1000 2000; do
