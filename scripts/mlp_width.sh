c6e8b35495ecb4a33ddc0e27dd6bf6d41996350c
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-18 19:34:54 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -29,6 +29,31 @@ for lr in "${LRS[@]}"; do
 done
 
 
+LRS=(-6.5 -6.25 -6 -5.75 -5.5 -5.25 -5) # SGD
+# 288 512 1152 2048 3200 4608 6272
+for lr in "${LRS[@]}"; do
+  for wid in 6272; do 
+    sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_approx \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension "$dim" \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_approx_ratio_1.txt"
+  done
+done
+
+
 # LRS=(-15 -10) # SGD
 # # 288 512 1152 2048 3200 4608
 # for wid in 512 1152 2048 3200 4608; do
