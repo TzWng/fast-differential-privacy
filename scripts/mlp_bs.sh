8958d1b6261f0c3a1de533ce3757a64f1c4644e3
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-24 12:53:30 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -82,7 +82,7 @@ BS=1024  # 你要的 batch size
 LRS=(-7.25 -8.5 -9)
 # 125 250 500 1000 2000
 for lr in "${LRS[@]}"; do
-  for BS in 250 500 1000 2000; do
+  for BS in 125; do
     epoch=$(( 4 * BS / 125 ))
     sig=$(awk "BEGIN {print 1*$BS/125.0}")
     echo "Running BS=$BS, lr=$lr, noise=$sig" 
