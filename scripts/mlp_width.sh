007b7bd0622ecfb8d3a3aeefadac8ebf4f887cce
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-24 10:28:11 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -28,10 +28,10 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-8 -8.5 -9) # SGD
+LRS=(-8.5 -8 -7.5 -7 -6.5) # SGD
 # 256 512 1024 2048 4096
-for lr in "${LRS[@]}"; do
-  for wid in 256; do 
+for wid in 512 1024 2048 4096; do 
+  for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 2.0*sqrt(256.0/$wid)}")
     echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
     $PYTHON -m scripts.MLP_muon_sgd \
