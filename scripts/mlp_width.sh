d4cab134a98b6eb735117aca72d4ad2fa4e2f15b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-24 10:42:30 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -28,9 +28,33 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-8.5 -8 -7.5 -7 -6.5) # SGD
+LRS=(-7.25) # SGD
 # 256 512 1024 2048 4096
-for wid in 512 1024 2048 4096; do 
+for wid in 256 512 1024 2048 4096; do 
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 2.0*sqrt(256.0/$wid)}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
+    $PYTHON -m scripts.MLP_muon_sgd \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Muon_depth5_diffwidth_approx_ratio_sgd_1.txt"
+  done
+done
+
+
+LRS=(-8.5 -8 -7.5 -7.25 -7 -6.5) # SGD
+# 256 512 1024 2048 4096
+for wid in 8192; do 
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 2.0*sqrt(256.0/$wid)}")
     echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
