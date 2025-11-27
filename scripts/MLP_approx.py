e04621929433517e84d6b0b95126c2f7b49d55ce
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-27 18:32:53 -0500

    Update MLP_approx.py

--- scripts/MLP_approx.py
+++ scripts/MLP_approx.py
@@ -290,7 +290,6 @@ def main(args):
 
 from fastDP import PrivacyEngine 
 import math, torch, os, torchvision 
-torch.manual_seed(2) 
 import torch.nn as nn 
 import torch.optim as optim 
 import torch.nn.functional as F 
@@ -329,5 +328,5 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
-    torch.manual_seed(2)
+    torch.manual_seed(0)
     main(args)
