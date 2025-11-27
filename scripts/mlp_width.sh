1ab01aa6a04d1c4830ad75b8236812d0247945bf
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-27 18:59:26 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,7 +5,7 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-6 -3 -2.5) # SGD
+LRS=(-2) # SGD
 # 288 512 1152 2048 3200 4608
 # 288 512 1152 2048 4608 8192
 for wid in 288 512 1152 2048 4608 8192; do 
@@ -33,30 +33,30 @@ done
 
 
 
-LRS=(-9 -8.5 -8 -7 -6.5 -6 -5.5 -5 -4.5) # SGD
-# 288 512 1152 2048 3200 4608
-# 288 512 1152 2048 4608 8192
-for wid in 288 512 1152 2048 4608 8192; do 
-  for lr in "${LRS[@]}"; do
-    sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
-    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-    $PYTHON -m scripts.MLP_approx \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise "$sig" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension "$dim" \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_approx_ratio_mup.txt"
-  done
-done
+# LRS=(-9 -8.5 -8 -7 -6.5 -6 -5.5 -5 -4.5) # SGD
+# # 288 512 1152 2048 3200 4608
+# # 288 512 1152 2048 4608 8192
+# for wid in 288 512 1152 2048 4608 8192; do 
+#   for lr in "${LRS[@]}"; do
+#     sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
+#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+#     $PYTHON -m scripts.MLP_approx \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 10 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise "$sig" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --cifar_data CIFAR10 \
+#       --dimension "$dim" \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_approx_ratio_mup.txt"
+#   done
+# done
 
 # LRS=(-4.5 -3.5 -2.5 -1.5) # SGD
 # # 288 512 1152 2048 3200 4608
