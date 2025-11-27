6ea6bf60212b92d850a1b5fa53f9bf66b1bf6db1
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-26 21:49:29 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -54,9 +54,34 @@ BS=1024  # 你要的 batch size
 #   done
 # done
 
-LRS=(-8 -7 -6 -5)
+LRS=(-8)
 # 125 250 500 1000 2000
-for BS in 250 500 1000 2000; do
+for BS in 125; do
+  for lr in "${LRS[@]}"; do
+    epoch=$(( 4 * BS / 125 ))
+    sig=$(awk "BEGIN {print 1*$BS/125.0}")
+    echo "Running BS=$BS, lr=$lr, noise=$sig" 
+    # scaled_lr=$(echo "$lr + $ratio" | bc -l)
+    $PYTHON -m scripts.MLP_sp \
+      --width 1024 \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs "$BS" \
+      --mini_bs "$BS" \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffbs_approx_ratio_sp.txt"
+  done
+done
+
+LRS=(-7.5 -6.5 -5.5 -4.5)
+# 125 250 500 1000 2000
+for BS in 125 250 500 1000 2000; do
   for lr in "${LRS[@]}"; do
     epoch=$(( 4 * BS / 125 ))
     sig=$(awk "BEGIN {print 1*$BS/125.0}")
