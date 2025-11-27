1fae6372af4024c185d6a4a4e9659b5efccc7a4e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-26 22:37:33 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -54,9 +54,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-6 -5 -4 -3 -2) # SGD
+LRS=(-5.5 -4.5 -3.5 -2.5) # SGD
 # 256 512 1024 2048 4096
-for wid in 512 1024 2048 4096; do 
+for wid in 256 512 1024 2048 4096; do 
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
     $PYTHON -m scripts.MLP_clipping_only \
