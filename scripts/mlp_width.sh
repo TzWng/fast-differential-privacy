012719e905df7703268589137e3cc07d1c819d1c
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-26 20:53:25 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,33 +4,11 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-8 -7.5 -7) # SGD
-# 288 512 1152 2048 3200 4608
-for wid in 288; do 
-  for lr in "${LRS[@]}"; do
-    sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
-    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-    $PYTHON -m scripts.MLP_approx \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise "$sig" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension "$dim" \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_approx_ratio_mup.txt"
-  done
-done
+
 
 LRS=(-8 -7.5 -7 -6.5 -6 -5.5 -4.5) # SGD
 # 288 512 1152 2048 3200 4608
-for wid in 512 2048 4608 8192; do 
+for wid in 288 512 2048 4608 8192; do 
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
