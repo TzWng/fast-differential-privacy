f86553bc1fade7e734d6d6c6da69a98684ce2df0
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-28 02:02:24 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,29 +5,29 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-# LRS=(-9 -8.5 -8 -7.5 -7 -6.5 -6) # SGD
-# # 288 512 1152 2048 4608 8192
-# for wid in 512 1152 2048 4608 8192; do 
-#   for lr in "${LRS[@]}"; do
-#     sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
-#     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
-#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-#     $PYTHON -m scripts.MLP_approx \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 20 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise "$sig" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --cifar_data CIFAR10 \
-#       --dimension "$dim" \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_approx_ratio_mup_3_ep20.txt"
-#   done
-# done
+LRS=(-9 -8.5 -8 -7.5 -7 -6.5 -6) # SGD
+# 288 512 1152 2048 4608 8192
+for wid in 512 1152 2048 4608 8192; do 
+  for lr in "${LRS[@]}"; do
+    sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_approx \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 20 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise "$sig" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension "$dim" \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_approx_ratio_mup_3_ep20.txt"
+  done
+done
 
 
 LRS=(-6.5 -6 -5.5 -5 -4.5 -4 -3.5 -3) # SGD
