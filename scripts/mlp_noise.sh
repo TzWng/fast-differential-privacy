39a12575566d90394332bf7ab03bbf027db3754b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-30 11:30:16 -0500

    Update mlp_noise.sh

--- scripts/mlp_noise.sh
+++ scripts/mlp_noise.sh
@@ -29,3 +29,29 @@ for wid in 3200; do
       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_change_bs.txt"
   done
 done
+
+LRS=(-5)
+# 128 512 2048 3200 8192
+for wid in 2048 8192; do
+  for lr in "${LRS[@]}"; do
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    bs=$(awk "BEGIN {print sqrt($wid/128.0)*125.0}")
+    epoch=$(( 4 * $bs / 125 ))
+    echo "Running width=$wid, dim=$dim, BS=$bs, lr=$lr" 
+    # scaled_lr=$(echo "$lr + $ratio" | bc -l)
+    $PYTHON -m scripts.MLP_approx \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs "$epoch"\
+      --bs "$bs" \
+      --mini_bs "$bs" \
+      --epsilon 2 \
+      --noise 1 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_diffwidth_change_bs.txt"
+  done
+done
