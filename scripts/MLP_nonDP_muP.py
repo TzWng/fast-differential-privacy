821ae2ead4017e8a061f7fc987116f7d882ab155
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-30 21:45:01 -0500

    Update MLP_nonDP_muP.py

--- scripts/MLP_nonDP_muP.py
+++ scripts/MLP_nonDP_muP.py
@@ -207,14 +207,15 @@ def main(args):
 
     # Model
     input_dim = 3 * args.dimension * args.dimension
-    # net = MLP(width=args.width, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256).to(device)
-    # Model
-    input_dim = 3 * args.dimension * args.dimension
-    base_model = MLP(width=128, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256)
-    delta_model = MLP(width=256, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256)
-    net = muMLP(width=args.width, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256).to(device)
-    net = net.to(device)
-    set_base_shapes(net, base_model, delta=delta_model)
+    net = MLP(width=args.width, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256).to(device)
+
+    
+    # input_dim = 3 * args.dimension * args.dimension
+    # base_model = MLP(width=128, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256)
+    # delta_model = MLP(width=256, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256)
+    # net = muMLP(width=args.width, input_dim=input_dim, nonlin=torch.relu, output_mult=32, input_mult=1/256).to(device)
+    # net = net.to(device)
+    # set_base_shapes(net, base_model, delta=delta_model)
 
     
 
@@ -334,5 +335,5 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
-    torch.manual_seed(3)
+    torch.manual_seed(4)
     main(args)
