7d2205907ad4e05ebf561070abe7f2b0cff5c050
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-30 19:44:44 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -24,6 +24,25 @@ for lr in "${LRS[@]}"; do
   done
 done
 
+LRS=(-4 -5) # SGD
+# 288 512 1152 2048 4608 8192
+for lr in "${LRS[@]}"; do
+  for wid in 512 1152 2048 4608 8192; do 
+    dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_nonDP_muP \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --cifar_data CIFAR10 \
+      --dimension "$dim" \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_nonDP_mup_3.txt"
+  done
+done
+
 # LRS=(-5.75) # SGD
 # # 288 512 1152 2048 4608 8192
 # for wid in 4608 8192; do 
