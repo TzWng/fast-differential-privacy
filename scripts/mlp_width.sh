84f0590d49066fbbbc2a6aee0e023c813916dc46
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-30 21:19:38 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,7 +5,7 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-2 -1.5 -1 -0.5 0) # SGD
+LRS=(-2 -2.5 -3 -3.5 -4 -4.5 -5 -5.5 -6 -6.5 -7) # SGD
 # 288 512 1152 2048 4608 8192
 for wid in 288 512 1152 2048 4608 8192; do 
   for lr in "${LRS[@]}"; do
@@ -20,7 +20,7 @@ for wid in 288 512 1152 2048 4608 8192; do
       --cifar_data CIFAR10 \
       --dimension "$dim" \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_nonDP_mup_4.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_nonDP_sp_3.txt"
   done
 done
 
