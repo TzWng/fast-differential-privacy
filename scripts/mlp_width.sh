eedf1cafe4536a7c85be0004055e3ffc0cbfb3b2
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-30 19:17:10 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -9,7 +9,6 @@ LRS=(--8 -7 -6 -5) # SGD
 # 288 512 1152 2048 4608 8192
 for wid in 288; do 
   for lr in "${LRS[@]}"; do
-    sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
     $PYTHON -m scripts.MLP_nonDP_muP \
@@ -18,10 +17,6 @@ for wid in 288; do
       --epochs 10 \
       --bs 500 \
       --mini_bs 500 \
-      --epsilon 2 \
-      --noise "$sig" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
       --cifar_data CIFAR10 \
       --dimension "$dim" \
       --optimizer SGD \
