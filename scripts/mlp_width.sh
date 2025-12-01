635fbf9ecfaf6a5d3b28d535902341a99125b3f8
TzWng <91708166+TzWng@users.noreply.github.com>
2025-11-30 23:55:23 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -26,7 +26,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-LRS=(-7 -6 -5) # SGD
+LRS=(-4 -8 -9) # SGD
 # 256 512 1024 2048 4096 8192
 for wid in 512 1024 2048 4096 8192; do 
   for lr in "${LRS[@]}"; do
@@ -45,6 +45,24 @@ for wid in 512 1024 2048 4096 8192; do
   done
 done
 
+LRS=(-4.5 -5.5 -6.5 -7.5 -8.5) # SGD
+# 256 512 1024 2048 4096 8192
+for wid in 256 512 1024 2048 4096 8192; do 
+  for lr in "${LRS[@]}"; do
+    dim=32
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
+    $PYTHON -m scripts.MLP_nonDP_muP \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer muon \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_muon_depth5_diffwidth_nonDP_lastlayer_sgd.txt"
+  done
+done
 
 # LRS=(-5.75) # SGD
 # # 288 512 1152 2048 4608 8192
