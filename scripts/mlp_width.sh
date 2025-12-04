b7f832331df90e818149d7e05ef856e8c8484292
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-04 18:59:10 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,9 +5,9 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-10.5 -10 -9.5 -9 -8.5 -8) # SGD
+LRS=(-7.5 -7) # SGD
 # 288 512 1152 2048 4608 8192
-for wid in 512 1024 2048; do 
+for wid in 512 do 
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr, dim=32"
     $PYTHON -m scripts.MLP_approx \
