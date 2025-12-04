3715c1b9762eb340696501839761dea3c0510c87
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-04 18:51:25 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,7 +5,7 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-9.5 -10 -10.5) # SGD
+LRS=(-9.5 -10 -10.5 -11) # SGD
 # 288 512 1152 2048 4608 8192
 for wid in 256; do 
   for lr in "${LRS[@]}"; do
