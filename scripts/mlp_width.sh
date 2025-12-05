39a725f8bfe0b0a2e30fa8c8bfdd6e55bd21abed
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-04 20:27:54 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -29,7 +29,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 LRS=(-8) # SGD
 # 288 512 1152 2048 4608 8192
-for wid in 512; do 
+for wid in 2048; do 
   for lr in "${LRS[@]}"; do
     sig=$(awk "BEGIN {print 2.0*sqrt(128.0/$wid)}")
     dim=$(awk "BEGIN {print sqrt($wid/128.0)*8.0}")
