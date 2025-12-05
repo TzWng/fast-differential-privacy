6552f837180c9ac5b3847a09769fadb6571c8e9d
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-04 19:28:49 -0500

    Update MLP_approx.py

--- scripts/MLP_approx.py
+++ scripts/MLP_approx.py
@@ -222,7 +222,8 @@ def main(args):
                     lr_scale = 1.0                  
                    
                     if grad is not None and grad.ndim in (1, 2):           
-                        # spec = torch.linalg.norm(grad, ord=2).clamp(min=eps) / args.bs
+                        spec = torch.linalg.norm(grad, ord=2).clamp(min=eps) / args.bs
+                        print("spectral norm is", spec)
                         # spec = torch.linalg.norm(grad, ord='fro').clamp(min=eps) / args.bs                       
                         if grad.ndim == 2:
                             if args.optimizer == 'SGD':
