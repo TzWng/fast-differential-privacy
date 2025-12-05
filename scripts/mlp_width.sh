48f74a39feb51b903f626fc13602f5429a63db21
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-04 19:37:47 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -39,7 +39,7 @@ for wid in 1152; do
       --bs 500 \
       --mini_bs 500 \
       --epsilon 2 \
-      --noise 0 \
+      --noise 2 \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --cifar_data CIFAR10 \
