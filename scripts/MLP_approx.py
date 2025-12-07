5caf64181191eb1a17c0f5de179a14d302787972
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-06 19:44:10 -0500

    Update MLP_approx.py

--- scripts/MLP_approx.py
+++ scripts/MLP_approx.py
@@ -140,7 +140,8 @@ def main(args):
 
     # f_i_k_vector = torch.zeros(L, dtype=torch.float32) + 1
     sum_term = torch.sum(1.0 / f_i_k_vector)
-    noise = args.noise * (sum_term / L)**(-0.5)
+    # noise = args.noise * (sum_term / L)**(-0.5)
+    noise = args.noise
     D_i_prime_vector = 1 / (f_i_k_vector * sum_term) ** 0.5
     print("clipping coefficient is", D_i_prime_vector)
    
