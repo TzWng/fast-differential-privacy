4eaf0d70d5d33b3034c14f932e5c29657446726b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-08 07:24:19 -0500

    Update MLP_approx.py

--- scripts/MLP_approx.py
+++ scripts/MLP_approx.py
@@ -236,6 +236,7 @@ def main(args):
                                     # lr_scale = (10 / param.shape[1]) ** 0.5 / a
                                     a = (128 ** 0.5) * args.noise / args.bs
                                     lr_scale = (10 / 128) ** 0.5 / a
+                                    lr_scale /= args.width / 128
                                 else:
                                     a = (param.shape[0] ** 0.5 + param.shape[1] ** 0.5) * noise / args.bs
                                     lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / a
