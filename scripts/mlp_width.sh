8a8c0197c335b5fd4a28670c2480e8c55b127cd4
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-07 23:05:53 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -91,10 +91,10 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-9 -9.5 -10) # SGD
+LRS=(-10) # SGD
 # 256 512 1024 2048 4096 8192
 for lr in "${LRS[@]}"; do
-  for wid in 256 512 1024 2048 4096 8192; do 
+  for wid in 8192; do 
     dim=32
     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
     $PYTHON -m scripts.MLP_nonDP_muP \
