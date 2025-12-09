bde5bea496417b5a53f0ad81018549ff60d5664b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-09 02:53:50 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -272,9 +272,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-LRS=(-10.5 -8) # SGD
+LRS=(-10.5 -10 -9.5 -9) # SGD
 # 256 512 1024 2048 4096
-for wid in 256 512 1024 2048 4096 8192; do 
+for wid in 256; do 
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
     $PYTHON -m scripts.MLP_muon \
@@ -290,7 +290,7 @@ for wid in 256 512 1024 2048 4096 8192; do
       --cifar_data CIFAR10 \
       --dimension 32 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Muon_depth5_diffwidth_approx_ratio_cl.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Muon_depth5_diffwidth_approx_new.txt"
   done
 done
 
