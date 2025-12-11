9278d75cda087514804d0efdf4548c43ebe20e60
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-11 06:23:48 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,7 +4,7 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-5.5) # SGD
+LRS=(-6 -5.5 -5) # SGD
 # 256 512 1024 2048 4096 8192
 for wid in 256; do 
   for lr in "${LRS[@]}"; do
@@ -26,27 +26,6 @@ for wid in 256; do
   done
 done
 
-LRS=(-5.5 -6 -6.5 -7) # SGD
-# 256 512 1024 2048 4096 8192
-for wid in 512 1024 2048 4096 8192; do 
-  for lr in "${LRS[@]}"; do
-    dim=32
-    echo "Running width=$wid, lr=$lr, dim=32"
-    $PYTHON -m scripts.dpmup_sgd \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --noise 2 \
-      --cifar_data CIFAR10 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/temp.txt"
-  done
-done
 
 # # LRS=(-5.5 -6) # SGD
 # LRS=(-6 -5.5 -5 -4.5 -4 -3.5 -3 -2.5 -2) # SGD
