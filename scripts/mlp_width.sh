28d3d1d5291df2c566605d3ea1c927b14c95efc5
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-11 23:31:36 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,28 +5,7 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-# LRS=(-4 -3.75 -3.5 -3.25 -3) # SGD
-# # 256 512 1024 2048 4096 8192
-# for lr in "${LRS[@]}"; do 
-#   for wid in 8192; do
-#     echo "Running width=$wid, lr=$lr, dim=32"
-#     $PYTHON -m scripts.dpmup_sgd \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 10 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --noise 2 \
-#       --cifar_data CIFAR10 \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --dimension 32 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/temp_0_8192.txt"
-#   done
-# done
-
-LRS=(-6 -5.5 -5 -4.5 -4 -3.5) # SGD
+LRS=(-2 -5 -5.5 -6) # SGD
 # 256 512 1024 2048 4096 8192
 for lr in "${LRS[@]}"; do 
   for wid in 256 512 1024 2048 4096 8192; do
@@ -43,9 +22,30 @@ for lr in "${LRS[@]}"; do
       --clipping_style layer-wise \
       --dimension 32 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/temp_sp.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/temp_0.txt"
   done
 done
+
+# LRS=(-6 -5.5 -5 -4.5 -4 -3.5) # SGD
+# # 256 512 1024 2048 4096 8192
+# for lr in "${LRS[@]}"; do 
+#   for wid in 256 512 1024 2048 4096 8192; do
+#     echo "Running width=$wid, lr=$lr, dim=32"
+#     $PYTHON -m scripts.dpmup_sgd \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 10 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --noise 2 \
+#       --cifar_data CIFAR10 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dimension 32 \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/temp_sp.txt"
+#   done
+# done
 
 
 # # LRS=(-5.5 -6) # SGD
