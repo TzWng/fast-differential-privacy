adb42ef448db8b89ef2bda4e135d15468ee9f285
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-12 04:03:08 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -8,7 +8,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 LRS=(-1.5 -6.5 -7 -2 -7.5 -8) # SGD
 # 256 512 1024 2048 4096 8192
 for lr in "${LRS[@]}"; do 
-  for wid in 256 512 1024 2048 4096 8192; do
+  for wid in 256; do
     echo "Running width=$wid, lr=$lr, dim=32"
     $PYTHON -m scripts.dpmup_sgd \
       --width "$wid" \
@@ -18,6 +18,28 @@ for lr in "${LRS[@]}"; do
       --mini_bs 500 \
       --noise 2 \
       --seed 4 \
+      --cifar_data CIFAR10 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/temp_1.txt"
+  done
+done
+
+LRS=(-1.5 -6.5 -7 -2 -7.5 -8) # SGD
+# 256 512 1024 2048 4096 8192
+for lr in "${LRS[@]}"; do 
+  for wid in 512 1024 2048 4096 8192; do
+    echo "Running width=$wid, lr=$lr, dim=32"
+    $PYTHON -m scripts.dpmup_sgd \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --noise 2 \
+      --seed 3 \
       --cifar_data CIFAR10 \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
