17675b41a213027dc7b40a08d8d7945ad7f424ec
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-12 02:14:31 -0500

    Update MLP_approx.py

--- scripts/MLP_approx.py
+++ scripts/MLP_approx.py
@@ -218,32 +218,32 @@ def main(args):
             
             if ((batch_idx + 1) % n_acc_steps == 0) or ((batch_idx + 1) == len(trainloader)):
                 
-                # for group in optimizer.param_groups:
-                #     name = group.get("name", "")
-                #     param = group["params"][0]
-                #     grad = param.private_grad
-                #     lr_scale = 1.0                  
+                for group in optimizer.param_groups:
+                    name = group.get("name", "")
+                    param = group["params"][0]
+                    grad = param.private_grad
+                    lr_scale = 1.0                  
                    
-                #     if grad is not None and grad.ndim in (1, 2):           
-                #         # spec = torch.linalg.norm(grad, ord=2).clamp(min=eps) / args.bs
-                #         # print("spectral norm is", spec)
-                #         # spec = torch.linalg.norm(grad, ord='fro').clamp(min=eps) / args.bs                       
-                #         if grad.ndim == 2:
-                #             if args.optimizer == 'SGD':
-                #                 # a = (param.shape[0] ** 0.5 + param.shape[1] ** 0.5) * args.noise / args.bs
-                #                 if param.shape[0] == 10: 
-                #                     a = (param.shape[1] ** 0.5) * noise / args.bs
-                #                     lr_scale = (10 / param.shape[1]) ** 0.5 / a
-                #                 else:
-                #                     a = (param.shape[0] ** 0.5 + param.shape[1] ** 0.5) * noise / args.bs
-                #                     lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / a
-                #             elif args.optimizer == 'Adam':
-                #                 a = (param.shape[0] ** 0.5 + param.shape[1] ** 0.5)
-                #                 lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / a
-                #         elif grad.ndim == 1:
-                #             lr_scale = (param.shape[0]) ** 0.5 / spec
+                    if grad is not None and grad.ndim in (1, 2):           
+                        # spec = torch.linalg.norm(grad, ord=2).clamp(min=eps) / args.bs
+                        # print("spectral norm is", spec)
+                        # spec = torch.linalg.norm(grad, ord='fro').clamp(min=eps) / args.bs                       
+                        if grad.ndim == 2:
+                            if args.optimizer == 'SGD':
+                                # a = (param.shape[0] ** 0.5 + param.shape[1] ** 0.5) * args.noise / args.bs
+                                if param.shape[0] == 10: 
+                                    a = (param.shape[1] ** 0.5) * noise / args.bs
+                                    lr_scale = (10 / param.shape[1]) ** 0.5 / a
+                                else:
+                                    a = (param.shape[0] ** 0.5 + param.shape[1] ** 0.5) * noise / args.bs
+                                    lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / a
+                            elif args.optimizer == 'Adam':
+                                a = (param.shape[0] ** 0.5 + param.shape[1] ** 0.5)
+                                lr_scale = (param.shape[0] / param.shape[1]) ** 0.5 / a
+                        elif grad.ndim == 1:
+                            lr_scale = (param.shape[0]) ** 0.5 / spec
                             
-                #     group["lr"] = base_lr * lr_scale
+                    group["lr"] = base_lr * lr_scale
 
                 optimizer.step()
                 optimizer.zero_grad()
