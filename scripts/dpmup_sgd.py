f678b8c1fed63bf41523dfddc86e1b61825fc4a4
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-11 19:38:06 -0500

    Update dpmup_sgd.py

--- scripts/dpmup_sgd.py
+++ scripts/dpmup_sgd.py
@@ -98,7 +98,7 @@ def main(args):
 
     base_lr = 2 ** args.lr
     # target_lr_dict = _get_lr4target(model_shapes, noise/args.bs, base_lr)
-    target_lr_dict = _get_lr4target(base_shapes, model_shapes, args.noise, noise, base_lr):
+    target_lr_dict = _get_lr4target(base_shapes, model_shapes, args.noise, noise, base_lr)
 
     param_groups = []
     for n, p in net.named_parameters():
