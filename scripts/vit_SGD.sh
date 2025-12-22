516f6b8e620b4e9c057979c4ef5d1cae6e398fea
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-22 04:17:06 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -16,7 +16,7 @@ MODELS=(
 
 LRS=(-3 -2.5 -2 -1.5 -1 -0.5 0)
 
-for s in 5; do
+for s in 1 4; do
   for lr in "${LRS[@]}"; do
     echo "Running scale=$s, lr=$lr"
     $PYTHON -m scripts.vit_unifed \
@@ -32,6 +32,6 @@ for s in 5; do
       --cifar_data CIFAR10 \
       --dimension 224 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_SGD_compare_3.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_SGD_compare_1.txt"
   done
 done
