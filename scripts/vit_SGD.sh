265878c62bac2302af9c65581c063d7d1d9e19c6
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-23 02:22:27 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -4,53 +4,53 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-# LRS=(-4)
-# for s in 1 2 3 4 5; do
-#   for lr in "${LRS[@]}"; do
-#     echo "Running scale=$s, lr=$lr"
-#     $PYTHON -m scripts.vit_unifed \
-#       --lr "$lr" \
-#       --epochs 3\
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise 2 \
-#       --scale "$s" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --dataset CIFAR10 \
-#       --dimension 224 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_SGD_compare_1.txt"
-#   done
-# done
-
-
-MODELS=(
-  vit_tiny_patch16_224
-  vit_small_patch16_224
-  vit_base_patch16_224
-)
-
-
-LRS=(-4 -3 -2 -1)
-for model in "${MODELS[@]}"; do
+LRS=(-3 -2 -1)
+for s in 1 2 3 4 5; do
   for lr in "${LRS[@]}"; do
-    echo "Running model=$model, lr=$lr"
+    echo "Running scale=$s, lr=$lr"
     $PYTHON -m scripts.vit_unifed \
-      --model "$model" \
       --lr "$lr" \
       --epochs 3\
       --bs 500 \
       --mini_bs 500 \
       --epsilon 2 \
       --noise 2 \
+      --scale "$s" \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --dataset CIFAR100 \
       --dimension 224 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_ft_cifar100_SGD_dpmup.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_SGD_compare_1.txt"
   done
 done
+
+
+# MODELS=(
+#   vit_tiny_patch16_224
+#   vit_small_patch16_224
+#   vit_base_patch16_224
+# )
+
+
+# LRS=(-4 -3 -2 -1)
+# for model in "${MODELS[@]}"; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running model=$model, lr=$lr"
+#     $PYTHON -m scripts.vit_unifed \
+#       --model "$model" \
+#       --lr "$lr" \
+#       --epochs 3\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR100 \
+#       --dimension 224 \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_ft_cifar100_SGD_dpmup.txt"
+#   done
+# done
 
