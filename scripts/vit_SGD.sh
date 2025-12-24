c243de1557bdaa3a06cd98111997743b5111090b
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-23 20:59:52 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -5,8 +5,29 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-3)
-for s in 5; do
+LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0 0.5)
+for s in 1 2 3 4 5; do
+  for lr in "${LRS[@]}"; do
+    echo "Running scale=$s, lr=$lr"
+    $PYTHON -m scripts.vit_unifed \
+      --lr "$lr" \
+      --epochs 5\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 2 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dataset CIFAR10 \
+      --dimension 224 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_SGD_compare_sp.txt"
+  done
+done
+
+LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0 0.5)
+for s in 1 2 3 4 5; do
   for lr in "${LRS[@]}"; do
     echo "Running scale=$s, lr=$lr"
     $PYTHON -m scripts.vit_unifed \
@@ -22,28 +43,7 @@ for s in 5; do
       --dataset CIFAR100 \
       --dimension 224 \
       --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_SGD_compare_3.txt"
-  done
-done
-
-LRS=(1 -3.5 -4)
-for lr in "${LRS[@]}"; do
-  for s in 1 2 3 4 5; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_unifed \
-      --lr "$lr" \
-      --epochs 5\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR100 \
-      --dimension 224 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_SGD_compare_3.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_SGD_compare_sp.txt"
   done
 done
 
