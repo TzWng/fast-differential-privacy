2d0cd3006efee040aeb6b34e96fd7cf06ccf5c8e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-24 07:34:36 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -27,28 +27,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 # done
 
 
-LRS=(-7 -8)
-for s in 1; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_muon_sgd\
-      --lr "$lr" \
-      --epochs 5\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR100 \
-      --dimension 224 \
-      --optimizer muon \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_muon.txt"
-  done
-done
-
-# LRS=(-7.5 -8.5)
+# LRS=(-7 -8)
 # for s in 1; do
 #   for lr in "${LRS[@]}"; do
 #     echo "Running scale=$s, lr=$lr"
@@ -68,6 +47,27 @@ done
 #       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_muon.txt"
 #   done
 # done
+
+LRS=(-7.5 -8.5)
+for s in 1; do
+  for lr in "${LRS[@]}"; do
+    echo "Running scale=$s, lr=$lr"
+    $PYTHON -m scripts.vit_muon_sgd\
+      --lr "$lr" \
+      --epochs 5\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 2 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dataset CIFAR100 \
+      --dimension 224 \
+      --optimizer muon \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_muon.txt"
+  done
+done
 
 # MODELS=(
 #   vit_tiny_patch16_224
