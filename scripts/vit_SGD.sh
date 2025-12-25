c2758f29f3ea32c33484b90f3f5ff9587adfd7a9
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-25 11:01:04 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -69,26 +69,6 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-2 -1.5 -1 -0.5 0 0.5)
-for s in 1; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_sp\
-      --lr "$lr" \
-      --epochs 5\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR100 \
-      --dimension 224 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_SGD_compare_sp.txt"
-  done
-done
 
 LRS=(-1.5 -2)
 for s in 4; do
