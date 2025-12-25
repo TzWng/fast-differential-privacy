84bd49daaefd899034d37b2cfe4d4e725d70a2c7
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-24 21:46:53 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -5,72 +5,50 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-5.25 -7.5 -8)
-for s in 1; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_muon_sgd\
-      --lr "$lr" \
-      --epochs 3\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR10 \
-      --dimension 224 \
-      --optimizer muon \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_muon.txt"
-  done
-done
+# LRS=(-5.25 -7.5 -8)
+# for s in 1; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_muon_sgd\
+#       --lr "$lr" \
+#       --epochs 3\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR10 \
+#       --dimension 224 \
+#       --optimizer muon \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_muon.txt"
+#   done
+# done
 
-LRS=(-5.25 -7.5)
-for s in 5; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_muon_sgd\
-      --lr "$lr" \
-      --epochs 3\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR10 \
-      --dimension 224 \
-      --optimizer muon \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_muon.txt"
-  done
-done
-
-LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0)
-for s in 2 3 4; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_sp\
-      --lr "$lr" \
-      --epochs 3\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR10 \
-      --dimension 224 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_SGD_compare_sp.txt"
-  done
-done
-
+# LRS=(-5.25 -7.5)
+# for s in 5; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_muon_sgd\
+#       --lr "$lr" \
+#       --epochs 3\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR10 \
+#       --dimension 224 \
+#       --optimizer muon \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_muon.txt"
+#   done
+# done
 
 # LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0)
-# for s in 5; do
+# for s in 2 3 4; do
 #   for lr in "${LRS[@]}"; do
 #     echo "Running scale=$s, lr=$lr"
 #     $PYTHON -m scripts.vit_sp\
@@ -86,9 +64,31 @@ done
 #       --dataset CIFAR10 \
 #       --dimension 224 \
 #       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_SGD_compare_sp_1.txt"
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_SGD_compare_sp.txt"
 #   done
 # done
+
+
+LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0)
+for s in 5; do
+  for lr in "${LRS[@]}"; do
+    echo "Running scale=$s, lr=$lr"
+    $PYTHON -m scripts.vit_sp\
+      --lr "$lr" \
+      --epochs 3\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 2 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dataset CIFAR10 \
+      --dimension 224 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_SGD_compare_sp_1.txt"
+  done
+done
 
 # LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0)
 # for s in 2 3 4 5; do
