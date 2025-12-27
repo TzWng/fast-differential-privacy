bb02fcdd2a5566506e2f3e8071ef4f10346d471d
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-27 00:28:49 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -28,28 +28,6 @@ export PYTHONPATH="$PROJECT_ROOT"
 # done
 
 
-LRS=(-2.5 -3 -3.5 -4 -4.5 -5 -5.5 -6 -6.5 -7 -7.5 -8 -8.5 -9) # SGD
-# 256 512 1024 2048 4096 8192
-for lr in "${LRS[@]}"; do 
-  for wid in 256; do
-    echo "Running width=$wid, lr=$lr, dim=32"
-    $PYTHON -m scripts.dpmup_sgd \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --noise 0.28948843479156494 \
-      --seed 3 \
-      --cifar_data CIFAR10 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/temp_seed_3.txt"
-  done
-done
-
 
 
 # LRS=(-6 -5.5 -5 -4.5 -4 -3.5) # SGD
@@ -140,24 +118,23 @@ done
 #   done
 # done
 
-# LRS=(-1.75) # SGD
-# # 256 512 1024 2048 4096 8192
-# for wid in 1024; do 
-#   for lr in "${LRS[@]}"; do
-#     dim=32
-#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=$dim"
-#     $PYTHON -m scripts.MLP_nonDP_muP \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 10 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --cifar_data CIFAR10 \
-#       --dimension 32 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/temp.txt"
-#   done
-# done
+LRS=(-6.5 -6 -5.5 -5 -4.5 -4 -3.5 -3) # SGD
+# 256 512 1024 2048 4096 8192
+for wid in 256 512 1024 2048 4096 8192; do 
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
+    $PYTHON -m scripts.MLP_nonDP_muP \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_nonDP_mup.txt"
+  done
+done
 
 # LRS=(-6.5) # SGD
 # # 256 512 1024 2048 4096 8192
