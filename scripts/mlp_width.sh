6b13d37740ee28dd34cb9b7d994e7db762da7ae2
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-26 22:59:24 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -5,27 +5,27 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-2.5 -2 -7.5 -8) # SGD
-# 256 512 1024 2048 4096 8192
-for lr in "${LRS[@]}"; do 
-  for wid in 512 1024 2048 4096; do
-    echo "Running width=$wid, lr=$lr, dim=32"
-    $PYTHON -m scripts.dpmup_sgd \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --noise 0.28948843479156494 \
-      --seed 3 \
-      --cifar_data CIFAR10 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_approx_ratio_dinfix_dpmup.txt"
-  done
-done
+# LRS=(-2.5 -2 -7.5 -8) # SGD
+# # 256 512 1024 2048 4096 8192
+# for lr in "${LRS[@]}"; do 
+#   for wid in 512 1024 2048 4096; do
+#     echo "Running width=$wid, lr=$lr, dim=32"
+#     $PYTHON -m scripts.dpmup_sgd \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 10 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --noise 0.28948843479156494 \
+#       --seed 3 \
+#       --cifar_data CIFAR10 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dimension 32 \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_approx_ratio_dinfix_dpmup.txt"
+#   done
+# done
 
 
 LRS=(-6 -5.5) # SGD
