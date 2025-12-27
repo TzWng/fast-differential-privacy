13f6e4131781c6c121ec8d0fc57cddaaaa915fce
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-27 02:08:29 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -118,26 +118,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-3 -3.5 -4 -4.5 -5 -5.5 -6 -6.5 -7) # SGD
-# 256 512 1024 2048 4096 8192
-for wid in 256 512 1024 2048 4096 8192; do 
-  for lr in "${LRS[@]}"; do
-    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
-    $PYTHON -m scripts.MLP_nonDP_muP \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_nonDP_sp_seed2.txt"
-  done
-done
-
-
-# LRS=(-4.5 -5 -5.5 -6 -6.5 -7) # SGD
+# LRS=(-3 -3.5 -4 -4.5 -5 -5.5 -6 -6.5 -7) # SGD
 # # 256 512 1024 2048 4096 8192
 # for wid in 256 512 1024 2048 4096 8192; do 
 #   for lr in "${LRS[@]}"; do
@@ -151,9 +132,28 @@ done
 #       --cifar_data CIFAR10 \
 #       --dimension 32 \
 #       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_nonDP_mup_seed2.txt"
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_nonDP_sp_seed2.txt"
 #   done
 # done
+
+
+LRS=(-4.75 -7.5 -8 -8.5 -9) # SGD
+# 256 512 1024 2048 4096 8192
+for wid in 256 512 1024 2048 4096 8192; do 
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
+    $PYTHON -m scripts.MLP_nonDP_muP \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_nonDP_mup_seed2.txt"
+  done
+done
 
 # LRS=(-6.5) # SGD
 # # 256 512 1024 2048 4096 8192
