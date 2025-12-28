e8e299df20f6553e30a2c03f33259a7cc9f23c8e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-28 03:29:38 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -30,26 +30,26 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-# LRS=(-6 -5.5 -5 -4.5 -4 -3.5) # SGD
-# # 256 512 1024 2048 4096 8192
-# for lr in "${LRS[@]}"; do 
-#   for wid in 256 512 1024 2048 4096 8192; do
-#     echo "Running width=$wid, lr=$lr, dim=32"
-#     $PYTHON -m scripts.dpmup_sgd \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 10 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --noise 2 \
-#       --cifar_data CIFAR10 \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --dimension 32 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/temp_sp.txt"
-#   done
-# done
+LRS=(-6 -5.5 -5 -4.5 -4 -3.5) # SGD
+# 256 512 1024 2048 4096 8192
+for lr in "${LRS[@]}"; do 
+  for wid in 256 512 1024 2048 4096 8192; do
+    echo "Running width=$wid, lr=$lr, dim=32"
+    $PYTHON -m scripts.dpmup_sgd \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --noise 2 \
+      --cifar_data CIFAR10 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/temp_sp.txt"
+  done
+done
 
 
 # # LRS=(-5.5 -6) # SGD
@@ -118,23 +118,23 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-2.5 -2) # SGD
-# 256 512 1024 2048 4096 8192
-for wid in 256; do 
-  for lr in "${LRS[@]}"; do
-    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
-    $PYTHON -m scripts.MLP_nonDP_muP \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_nonDP_sp_seed2.txt"
-  done
-done
+# LRS=(-2.5 -2) # SGD
+# # 256 512 1024 2048 4096 8192
+# for wid in 256; do 
+#   for lr in "${LRS[@]}"; do
+#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
+#     $PYTHON -m scripts.MLP_nonDP_muP \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 10 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --cifar_data CIFAR10 \
+#       --dimension 32 \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_l2s_nonDP_sp_seed2.txt"
+#   done
+# done
 
 
 # LRS=(-4.75 -7.5 -8 -8.5 -9) # SGD
