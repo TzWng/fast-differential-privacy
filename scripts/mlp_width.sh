c7026d3ca5b07b8402294a56bd2ac4b1f907c23f
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-28 07:48:25 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -52,9 +52,31 @@ export PYTHONPATH="$PROJECT_ROOT"
 # done
 
 
-LRS=(-5.5 -5 -4.5 -4 -3.5) # SGD
+LRS=(-3.5) # SGD
 # 256 512 1024 2048 4096 8192
-for wid in 512 1024 2048 4096 8192; do
+for wid in 256; do
+  for lr in "${LRS[@]}"; do 
+    echo "Running width=$wid, lr=$lr, dim=32"
+    $PYTHON -m scripts.dpmup_sgd \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --noise 7.067494947837502 \
+      --seed 4 \
+      --cifar_data CIFAR10 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dimension 32 \
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_s2l_epsilon2_dinfix_dpmup.txt"
+  done
+done
+
+LRS=(-3 -2.5) # SGD
+# 256 512 1024 2048 4096 8192
+for wid in 256 512 1024 2048 4096 8192; do
   for lr in "${LRS[@]}"; do 
     echo "Running width=$wid, lr=$lr, dim=32"
     $PYTHON -m scripts.dpmup_sgd \
@@ -76,27 +98,27 @@ done
 
 
 
-LRS=(-11 -10.5 -10 -9.5 -9 -8.5 -8 -7.5 -7 -6.5 -6 -5.5) # SGD
-# 256 512 1024 2048 4096 8192
-for wid in 256 512 1024 2048 4096 8192; do
-  for lr in "${LRS[@]}"; do 
-    echo "Running width=$wid, lr=$lr, dim=32"
-    $PYTHON -m scripts.dpmup_sgd \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --noise 7.067494947837502 \
-      --seed 4 \
-      --cifar_data CIFAR10 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dimension 32 \
-      --optimizer muon \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_muonsgd_depth5_s2l_epsilon2_dinfix_dpmup.txt"
-  done
-done
+# LRS=(-11 -10.5 -10 -9.5 -9 -8.5 -8 -7.5 -7 -6.5 -6 -5.5) # SGD
+# # 256 512 1024 2048 4096 8192
+# for wid in 256 512 1024 2048 4096 8192; do
+#   for lr in "${LRS[@]}"; do 
+#     echo "Running width=$wid, lr=$lr, dim=32"
+#     $PYTHON -m scripts.dpmup_sgd \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 10 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --noise 7.067494947837502 \
+#       --seed 4 \
+#       --cifar_data CIFAR10 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dimension 32 \
+#       --optimizer muon \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_muonsgd_depth5_s2l_epsilon2_dinfix_dpmup.txt"
+#   done
+# done
 
 
 
