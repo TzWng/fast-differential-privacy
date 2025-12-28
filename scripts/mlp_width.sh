40627fac5fa66f5747844418d2c80d612f9a32a4
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-28 07:58:58 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -52,27 +52,6 @@ export PYTHONPATH="$PROJECT_ROOT"
 # done
 
 
-LRS=(-8.5 -9) # SGD
-# 256 512 1024 2048 4096 8192
-for wid in 512; do
-  for lr in "${LRS[@]}"; do 
-    echo "Running width=$wid, lr=$lr, dim=32"
-    $PYTHON -m scripts.dpmup_sgd \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --noise 7.067494947837502 \
-      --seed 4 \
-      --cifar_data CIFAR10 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_s2l_epsilon2_dinfix_dpmup.txt"
-  done
-done
 
 LRS=(-6 -6.5 -7 -7.5 -8 -8.5 -9) # SGD
 # 256 512 1024 2048 4096 8192
