2b6464d8ba1e9151edde8802f92c371c25ff3a09
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-29 08:45:05 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -3,10 +3,34 @@
 PYTHON=python3.10
 BS=1024  # 你要的 batch size
 
-# batch size 500 epsilon=2
-LRS=(-6 -5.5 -5 -4.5 -4 -3.5 -3) # SGD
+# # batch size 500 epsilon=2
+# LRS=(-6 -5.5 -5 -4.5 -4 -3.5 -3) # SGD
+# # 256 512 1024 2048 4096 8192
+# for BS in 250 500 1000 2000; do
+#   for lr in "${LRS[@]}"; do 
+#     epoch=$(( 4 * BS / 125 ))
+#     sig=$(awk "BEGIN {print 3.586439615946674*$BS/500.0}")
+#     echo "Running batch size=$BS, lr=$lr, noise=$sig, epoch=$epoch"
+#     $PYTHON -m scripts.dpmup_sgd \
+#       --width 2048 \
+#       --lr "$lr" \
+#       --epochs "$epoch" \
+#       --bs "$BS" \
+#       --mini_bs "$BS" \
+#       --noise "$sig" \
+#       --seed 3 \
+#       --cifar_data CIFAR10 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dimension 32 \
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffbs_dinfix_dpmup.txt"
+#   done
+# done
+
+LRS=(-9 -8.5) # SGD
 # 256 512 1024 2048 4096 8192
-for BS in 250 500 1000 2000; do
+for BS in 125; do
   for lr in "${LRS[@]}"; do 
     epoch=$(( 4 * BS / 125 ))
     sig=$(awk "BEGIN {print 3.586439615946674*$BS/500.0}")
@@ -23,14 +47,13 @@ for BS in 250 500 1000 2000; do
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffbs_dinfix_dpmup.txt"
+      --optimizer muon \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_muon_depth5_diffbs_dinfix_dpmup.txt"
   done
 done
 
 
-
-LRS=(-8 -7.5 -7 -6.5 -6) # SGD
+LRS=(-9 -8.5 -8 -7.5 -7 -6.5 -6) # SGD
 # 256 512 1024 2048 4096 8192
 for BS in 250 500 1000 2000; do
   for lr in "${LRS[@]}"; do 
