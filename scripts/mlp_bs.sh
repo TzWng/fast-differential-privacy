de965735da67198e84c40d49dbfe3a4c50e9b891
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-29 08:31:18 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -8,7 +8,7 @@ LRS=(-6 -5.5 -5 -4.5 -4 -3.5 -3) # SGD
 for BS in 500; do
   for lr in "${LRS[@]}"; do 
     epoch=$(( 4 * BS / 125 ))
-    sig=$(awk "BEGIN {print 4.417184342398438*$BS/500.0}")
+    sig=$(awk "BEGIN {print 3.586439615946674*$BS/500.0}")
     echo "Running width=2048, lr=$lr, noise=$sig, epoch=$epoch"
     $PYTHON -m scripts.dpmup_sgd \
       --width 2048 \
