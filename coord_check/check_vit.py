b598b984ac34809c037f5d24990ecd6a41577a5d
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-29 02:33:57 -0500

    Update check_vit.py

--- coord_check/check_vit.py
+++ coord_check/check_vit.py
@@ -95,8 +95,8 @@ def my_custom_optimizer_fn(net, args, trainset_len, mode='full'):
             "name": n
         })
     
-    # optimizer = optim.SGD(param_groups, lr=base_lr)
-    optimizer = optim.SGD(net.parameters(), lr=base_lr)
+    optimizer = optim.SGD(param_groups, lr=base_lr)
+    # optimizer = optim.SGD(net.parameters(), lr=base_lr)
 
     # 6. Privacy Engine
     if 'nonDP' not in args.clipping_mode:
@@ -112,10 +112,10 @@ def my_custom_optimizer_fn(net, args, trainset_len, mode='full'):
             net,
             batch_size=args.bs,
             sample_size=trainset_len,
-            noise_multiplier=args.noise,
+            noise_multiplier=noise,
             epochs=args.epochs,
             clipping_mode=clipping_mode,
-            # clipping_coe=D_prime_vector, 
+            clipping_coe=D_prime_vector, 
             clipping_style=args.clipping_style,
             origin_params=args.origin_params,
         )
@@ -292,7 +292,7 @@ def coord_check_split_terms(lr, model_fn, optimizer_fn, batch_size, nsteps, nsee
             return model
         return f
 
-    scales = np.arange(1, 4) 
+    scales = np.arange(1, 7) 
     models = {int(s): gen(int(s)) for s in scales}
                       
     transformation = torchvision.transforms.Compose([
@@ -340,6 +340,6 @@ coord_check_split_terms(
     optimizer_fn=my_custom_optimizer_fn,
     batch_size=args.mini_bs,
     nsteps=4,
-    nseeds=1,
+    nseeds=3,
     args=args
 )
