349ac283e9be448dbb4bb7e53ca05313d3a50fde
TzWng <91708166+TzWng@users.noreply.github.com>
2025-12-29 03:10:32 -0500

    Update check_vit.py

--- coord_check/check_vit.py
+++ coord_check/check_vit.py
@@ -267,17 +267,6 @@ def setprec(model, precision='float32'):
 
 
 def coord_check_split_terms(lr, model_fn, optimizer_fn, batch_size, nsteps, nseeds, args):
-
-    def kaiming_init_weights(m):
-        if isinstance(m, nn.Linear):
-            if m is net.head:
-                nn.init.zeros_(m.weight)
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            else:
-                nn.init.kaiming_normal_(m.weight, a=1, mode='fan_in')
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
                     
     def gen(s):
         def f():
@@ -288,6 +277,17 @@ def coord_check_split_terms(lr, model_fn, optimizer_fn, batch_size, nsteps, nsee
             model_wrapper = MyVit(local_args, is_base=False)
             net = model_wrapper.create_model()
 
+            def kaiming_init_weights(m):
+                if isinstance(m, nn.Linear):
+                    if m is net.head:
+                        nn.init.zeros_(m.weight)
+                        if m.bias is not None:
+                            nn.init.zeros_(m.bias)
+                    else:
+                        nn.init.kaiming_normal_(m.weight, a=1, mode='fan_in')
+                        if m.bias is not None:
+                            nn.init.zeros_(m.bias)
+                    
             net.apply(kaiming_init_weights)
             
             net = setprec(net, args.precision)
