fdbe8088dd8b4e7b36aa0a14c9273d8191e01700
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-01 22:55:41 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -4,36 +4,13 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-
-
-# LRS=(-11 -10.5 -10 -9.5 -9 -8.5 -8)
-# for s in 2 3 4 5; do
-#   for lr in "${LRS[@]}"; do
-#     echo "Running scale=$s, lr=$lr"
-#     $PYTHON -m scripts.vit_unifed\
-#       --lr "$lr" \
-#       --epochs 3\
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise 2 \
-#       --scale "$s" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --dataset CIFAR10 \
-#       --dimension 224 \
-#       --optimizer Adam \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_adam.txt"
-#   done
-# done
-
-LRS=(-11 -10.5 -10 -9.5 -9 -8.5 -8 -7.5)
-for s in 1 2 3 4 5; do
+LRS=(-7.5)
+for s in 1; do
   for lr in "${LRS[@]}"; do
     echo "Running scale=$s, lr=$lr"
     $PYTHON -m scripts.vit_unifed\
       --lr "$lr" \
-      --epochs 5\
+      --epochs 3\
       --bs 500 \
       --mini_bs 500 \
       --epsilon 2 \
@@ -41,12 +18,54 @@ for s in 1 2 3 4 5; do
       --scale "$s" \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
-      --dataset CIFAR100 \
+      --dataset CIFAR10 \
       --dimension 224 \
       --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_adam.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_adam.txt"
   done
 done
+
+LRS=(-7.5 -7)
+for s in 1 2 3 4 5; do
+  for lr in "${LRS[@]}"; do
+    echo "Running scale=$s, lr=$lr"
+    $PYTHON -m scripts.vit_unifed\
+      --lr "$lr" \
+      --epochs 3\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 2 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dataset CIFAR10 \
+      --dimension 224 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_adam.txt"
+  done
+done
+
+# LRS=(-11 -10.5 -10 -9.5 -9 -8.5 -8 -7.5)
+# for s in 1 2 3 4 5; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_unifed\
+#       --lr "$lr" \
+#       --epochs 5\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR100 \
+#       --dimension 224 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_adam.txt"
+#   done
+# done
 
 # LRS=(-5.25 -7.5)
 # for s in 5; do
