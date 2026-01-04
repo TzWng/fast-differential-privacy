af5ae5932876e115f38938cc61f65b2d5ce0754d
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-04 02:50:03 -0500

    Update train_DP.py

--- train_DP.py
+++ train_DP.py
@@ -45,7 +45,7 @@ init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 wandb_log = True # disabled by default
 wandb_project = 'DPscaling'
 # data
-dataset = 'openwebtext'
+dataset = 'shakespeare_char'
 gradient_accumulation_steps = 8 # used to simulate larger batch sizes
 batch_size = 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
