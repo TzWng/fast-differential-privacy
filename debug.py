43ecc0e35c910d01266fb9440ad696b5264df97f
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-04 23:31:40 -0500

    Update debug.py

--- debug.py
+++ debug.py
@@ -59,7 +59,7 @@ n_head = 20
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
-learning_rate = 2e-3 # max learning rate
+learning_rate = 4e-3 # max learning rate
 total_compute = 1e16
 weight_decay = 1e-1
 beta1 = 0.9
