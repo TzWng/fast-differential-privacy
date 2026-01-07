99251891961db8a61194ea47fb2372591ddcf340
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-07 12:48:47 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,27 +4,27 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-# LRS=(-15 -14 -13 -12 -11) # SGD
-# # 256 512 1024 2048 4096 8192
-# for wid in 256; do 
-#   for lr in "${LRS[@]}"; do
-#     echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
-#     $PYTHON -m scripts.MLP_clipping_only \
-#       --width "$wid" \
-#       --lr "$lr" \
-#       --epochs 10 \
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise 0 \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --cifar_data CIFAR10 \
-#       --dimension 32 \
-#       --optimizer Adam \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_depth5_diffwidth_true_fail.txt"
-#   done
-# done
+LRS=(-16 -17 -18) # SGD
+# 256 512 1024 2048 4096 8192
+for wid in 256; do 
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_depth5_diffwidth_true_fail.txt"
+  done
+done
 
 LRS=(-9 -8 -7 -6 -5) # SGD
 # 256 512 1024 2048 4096 8192
