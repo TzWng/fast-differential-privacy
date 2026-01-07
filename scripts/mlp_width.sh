1925d7d4ed562a40e0239ac04ec5926170de37a4
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-07 00:38:59 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -144,9 +144,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-LRS=(-12 -11 -10) # SGD
+LRS=(-13 -12 -11 -10) # SGD
 # 256 512 1024 2048 4096 8192
-for wid in 512 1024 2048 4096 8192; do 
+for wid in 256; do 
   for lr in "${LRS[@]}"; do
     echo "Running width=$wid, lr=$lr, dim=32"
     $PYTHON -m scripts.dpmup_adam \
