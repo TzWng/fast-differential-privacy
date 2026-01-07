9c24a41c6b89d36bb36df4f8d8c6660ed78743ce
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-06 21:33:19 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -120,7 +120,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-14.5 -14 -13.5) # SGD
+LRS=(-15.5) # SGD
 # 256 512 1024 2048 4096 8192
 for wid in 256; do
   for lr in "${LRS[@]}"; do 
