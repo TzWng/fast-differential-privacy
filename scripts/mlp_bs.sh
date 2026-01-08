94ef8212435449b2c535ab950ce8ad394987ebd8
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-08 04:24:02 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -4,9 +4,9 @@ PYTHON=python3.10
 BS=1024  # 你要的 batch size
 
 # batch size 500 epsilon=2
-LRS=(-13 -12.5 -11.5 -10.5) # SGD
+LRS=(-13 -12.5 -12 -11.5 -11 -10.5 -10) # SGD
 # 256 512 1024 2048 4096 8192
-for BS in 125; do
+for BS in 250 500 1000 2000; do
   for lr in "${LRS[@]}"; do 
     epoch=$(( 4 * BS / 125 ))
     sig=$(awk "BEGIN {print 3.586439615946674*$BS/500.0}")
