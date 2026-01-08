c763358cb19065cc6566237a4219e022252fafe5
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-08 04:07:45 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -4,47 +4,47 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-LRS=(-7.5)
-for s in 1; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_unifed\
-      --lr "$lr" \
-      --epochs 3\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR10 \
-      --dimension 224 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_adam.txt"
-  done
-done
+# LRS=(-7.5)
+# for s in 1; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_unifed\
+#       --lr "$lr" \
+#       --epochs 3\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR10 \
+#       --dimension 224 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_adam.txt"
+#   done
+# done
 
-LRS=(-7.5 -7)
-for s in 1 2 3 4 5; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_unifed\
-      --lr "$lr" \
-      --epochs 3\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR10 \
-      --dimension 224 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_adam.txt"
-  done
-done
+# LRS=(-7.5 -7)
+# for s in 1 2 3 4 5; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_unifed\
+#       --lr "$lr" \
+#       --epochs 3\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR10 \
+#       --dimension 224 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_adam.txt"
+#   done
+# done
 
 # LRS=(-11 -10.5 -10 -9.5 -9 -8.5 -8 -7.5)
 # for s in 1 2 3 4 5; do
@@ -88,49 +88,28 @@ done
 #   done
 # done
 
-# LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0)
-# for s in 2 3 4; do
-#   for lr in "${LRS[@]}"; do
-#     echo "Running scale=$s, lr=$lr"
-#     $PYTHON -m scripts.vit_sp\
-#       --lr "$lr" \
-#       --epochs 3\
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise 2 \
-#       --scale "$s" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --dataset CIFAR10 \
-#       --dimension 224 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_SGD_compare_sp.txt"
-#   done
-# done
+LRS=(-7 -6 -5)
+for s in 1; do
+  for lr in "${LRS[@]}"; do
+    echo "Running scale=$s, lr=$lr"
+    $PYTHON -m scripts.vit_sp\
+      --lr "$lr" \
+      --epochs 5\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 2 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dataset CIFAR100 \
+      --dimension 224 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_Adam_compare_sp.txt"
+  done
+done
 
 
-
-# LRS=(-3.5 -3 -2.5 -2 -1.5 -1 -0.5 0)
-# for s in 1; do
-#   for lr in "${LRS[@]}"; do
-#     echo "Running scale=$s, lr=$lr"
-#     $PYTHON -m scripts.vit_sp\
-#       --lr "$lr" \
-#       --epochs 3\
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise 2 \
-#       --scale "$s" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --dataset CIFAR10 \
-#       --dimension 224 \
-#       --optimizer SGD \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_SGD_compare_sp.txt"
-#   done
-# done
 
 
 
