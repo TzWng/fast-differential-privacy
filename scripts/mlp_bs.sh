18bc24d038b7519b6a8b3c217d1cedf11e6ec887
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-08 05:47:14 -0500

    Update mlp_bs.sh

--- scripts/mlp_bs.sh
+++ scripts/mlp_bs.sh
@@ -79,34 +79,33 @@ BS=1024  # 你要的 batch size
 # done
 
 
-
-LRS=(-12 -11.5 -11 -10.5) # SGD
-# 256 512 1024 2048 4096 8192
-for BS in 100; do
-  for lr in "${LRS[@]}"; do 
-    epoch=$(( 2 * BS / 50 ))
-    sig=$(awk "BEGIN {print 1.4983855926238738*$BS/400.0}")
-    echo "Running batch size=$BS, lr=$lr, noise=$sig, epoch=$epoch"
-    $PYTHON -m scripts.vit_unifed \
-      --lr "$lr" \
-      --epochs "$epoch" \
-      --bs "$BS" \
-      --mini_bs "$BS" \
-      --noise "$sig" \
-      --scale 1 \
-      --dataset CIFAR10 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dimension 224 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_Adam_diffbs_dinfix_dpmup.txt"
-  done
-done
-
+# LRS=(-13 -12.5 -12 -11.5 -11 -10.5) # SGD
+# # 256 512 1024 2048 4096 8192
+# for BS in 200 400; do
+#   for lr in "${LRS[@]}"; do 
+#     epoch=$(( 2 * BS / 50 ))
+#     sig=$(awk "BEGIN {print 1.4983855926238738*$BS/400.0}")
+#     echo "Running batch size=$BS, lr=$lr, noise=$sig, epoch=$epoch"
+#     $PYTHON -m scripts.vit_unifed \
+#       --lr "$lr" \
+#       --epochs "$epoch" \
+#       --bs "$BS" \
+#       --mini_bs "$BS" \
+#       --noise "$sig" \
+#       --scale 1 \
+#       --dataset CIFAR10 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dimension 224 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_Adam_diffbs_dinfix_dpmup.txt"
+#   done
+# done
 
 LRS=(-13 -12.5 -12 -11.5 -11 -10.5) # SGD
+LRS=(-10.5 -11 -11.5 -12 -12.5 -13) # SGD
 # 256 512 1024 2048 4096 8192
-for BS in 200 400; do
+for BS in 400; do
   for lr in "${LRS[@]}"; do 
     epoch=$(( 2 * BS / 50 ))
     sig=$(awk "BEGIN {print 1.4983855926238738*$BS/400.0}")
