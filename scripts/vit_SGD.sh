cb0589ed74ad0f8473b41e3c37914e6b5f17df8d
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-08 07:48:16 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -27,26 +27,26 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-# LRS=(-9 -8.5 -8 -7.5)
-# for s in 2; do
-#   for lr in "${LRS[@]}"; do
-#     echo "Running scale=$s, lr=$lr" 
-#     $PYTHON -m scripts.vit_sp\
-#       --lr "$lr" \
-#       --epochs 5\
-#       --bs 500 \
-#       --mini_bs 500 \
-#       --epsilon 2 \
-#       --noise 2 \
-#       --scale "$s" \
-#       --clipping_mode BK-MixOpt \
-#       --clipping_style layer-wise \
-#       --dataset CIFAR100 \
-#       --dimension 224 \
-#       --optimizer Adam \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_Adam_compare_sp_1.txt"
-#   done
-# done
+LRS=(-9 -8.5 -8 -7.5)
+for s in 2; do
+  for lr in "${LRS[@]}"; do
+    echo "Running scale=$s, lr=$lr" 
+    $PYTHON -m scripts.vit_sp\
+      --lr "$lr" \
+      --epochs 5\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 2 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dataset CIFAR100 \
+      --dimension 224 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_Adam_compare_sp_1.txt"
+  done
+done
 
 # LRS=(-10.5 -10 -9.5 -9 -8.5 -8 -7.5)
 # for s in 3 4 5; do
@@ -71,27 +71,27 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-LRS=(-12 -11.5 -11 -10.5 -10 -9.5 -9)
-LRS=(-9 -9.5 -10 -10.5 -11 -11.5 -12)
-for s in 5; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_sp\
-      --lr "$lr" \
-      --epochs 3\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR10 \
-      --dimension 224 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_Adam_compare_sp_1.txt"
-  done
-done
+# LRS=(-12 -11.5 -11 -10.5 -10 -9.5 -9)
+# LRS=(-9 -9.5 -10 -10.5 -11 -11.5 -12)
+# for s in 5; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_sp\
+#       --lr "$lr" \
+#       --epochs 3\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR10 \
+#       --dimension 224 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_Adam_compare_sp_1.txt"
+#   done
+# done
 
 
 
