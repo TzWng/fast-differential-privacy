d757e78c592013d3d6f9f07d9c066bbf8c0289fc
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-08 05:10:52 -0500

    Update vit_SGD.sh

--- scripts/vit_SGD.sh
+++ scripts/vit_SGD.sh
@@ -5,6 +5,29 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
+LRS=(-10.5 -10 -9.5 -9 -8.5 -8 -7.5)
+LRS=(-7.5 -8 -8.5 -9 -9.5 -10 -10.5)
+for s in 5; do
+  for lr in "${LRS[@]}"; do
+    echo "Running scale=$s, lr=$lr" 
+    $PYTHON -m scripts.vit_sp\
+      --lr "$lr" \
+      --epochs 5\
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 2 \
+      --scale "$s" \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --dataset CIFAR100 \
+      --dimension 224 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar100_Adam_compare_sp.txt"
+  done
+done
+
+
 # LRS=(-10.5 -10 -9.5 -9 -8.5 -8 -7.5)
 # for s in 2 3 4 5; do
 #   for lr in "${LRS[@]}"; do
@@ -28,26 +51,26 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-LRS=(-12 -11.5 -11 -10.5 -10 -9.5 -9 -8.5)
-for s in 3 4 5; do
-  for lr in "${LRS[@]}"; do
-    echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_sp\
-      --lr "$lr" \
-      --epochs 3\
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 2 \
-      --scale "$s" \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --dataset CIFAR10 \
-      --dimension 224 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_Adam_compare_sp.txt"
-  done
-done
+# LRS=(-12 -11.5 -11 -10.5 -10 -9.5 -9 -8.5)
+# for s in 3 4 5; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_sp\
+#       --lr "$lr" \
+#       --epochs 3\
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR10 \
+#       --dimension 224 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/Vit_cifar10_Adam_compare_sp.txt"
+#   done
+# done
 
 
 # LRS=(-7.5)
