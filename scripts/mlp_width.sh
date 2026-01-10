dbddcbbe8f9e7d8788a4b6931a4ddd039963aef4
TzWng <91708166+TzWng@users.noreply.github.com>
2026-01-10 01:02:57 -0500

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -4,7 +4,30 @@ PYTHON=python3.10
 PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
-# LRS=(-2 -1.5 -1 -0.5 0 0.5 1) # SGD
+LRS=(-2 -1.5 -1 -0.5 0 0.5 1) # SGD
+# 256 512 1024 2048 4096 8192
+for wid in 512 1024 2048 4096; do 
+  for lr in "${LRS[@]}"; do
+    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
+    $PYTHON -m scripts.MLP_clipping_only \
+      --width "$wid" \
+      --lr "$lr" \
+      --epochs 10 \
+      --bs 500 \
+      --mini_bs 500 \
+      --epsilon 2 \
+      --noise 0 \
+      --clipping_mode BK-MixOpt \
+      --clipping_style layer-wise \
+      --cifar_data CIFAR10 \
+      --dimension 32 \
+      --optimizer Adam \
+      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_depth5_diffwidth_co.txt"
+  done
+done
+
+
+# LRS=(-4 -3.5 -3 -2.5 -2 -1.5 -1 -0.5 0) # SGD
 # # 256 512 1024 2048 4096 8192
 # for wid in 256; do 
 #   for lr in "${LRS[@]}"; do
@@ -21,33 +44,10 @@ export PYTHONPATH="$PROJECT_ROOT"
 #       --clipping_style layer-wise \
 #       --cifar_data CIFAR10 \
 #       --dimension 32 \
-#       --optimizer Adam \
-#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_depth5_diffwidth_co.txt"
+#       --optimizer SGD \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_co.txt"
 #   done
 # done
-
-
-LRS=(-4 -3.5 -3 -2.5 -2 -1.5 -1 -0.5 0) # SGD
-# 256 512 1024 2048 4096 8192
-for wid in 256; do 
-  for lr in "${LRS[@]}"; do
-    echo "Running width=$wid, lr=$lr, noise=$sig, dim=32"
-    $PYTHON -m scripts.MLP_clipping_only \
-      --width "$wid" \
-      --lr "$lr" \
-      --epochs 10 \
-      --bs 500 \
-      --mini_bs 500 \
-      --epsilon 2 \
-      --noise 0 \
-      --clipping_mode BK-MixOpt \
-      --clipping_style layer-wise \
-      --cifar_data CIFAR10 \
-      --dimension 32 \
-      --optimizer SGD \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_SGD_depth5_diffwidth_co.txt"
-  done
-done
 
 
 # LRS=(-13 -14 -15 -16 -17 -18) # SGD
