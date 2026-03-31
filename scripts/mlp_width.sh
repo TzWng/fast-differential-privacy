055e04cf6f634e8626d18c4ce9ce96b305d3c52e
TzWng <91708166+TzWng@users.noreply.github.com>
2026-03-31 19:40:17 -0400

    Change width parameter from 256 to 512 and optimizer to SGD

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -7,7 +7,7 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 LRS=(0) # SGD
 # 256 512 1024 2048 4096 8192
-for wid in 256; do
+for wid in 512; do
   for lr in "${LRS[@]}"; do 
     echo "Running width=$wid, lr=$lr, dim=32"
     $PYTHON -m scripts.MLP_clipping_only \
@@ -21,7 +21,7 @@ for wid in 256; do
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --dimension 32 \
-      --optimizer Adam \
+      --optimizer SGD \
       --log_path "/content/drive/MyDrive/DP_muP/logs/temp.txt"
   done
 done
