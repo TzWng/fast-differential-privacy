cc548787ebda7a24d63e33f220bbc7094f7c7b96
TzWng <91708166+TzWng@users.noreply.github.com>
2026-04-29 21:33:05 -0400

    Update mlp_width.sh

--- scripts/mlp_width.sh
+++ scripts/mlp_width.sh
@@ -7,15 +7,15 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 LRS=(0) # SGD
 # 256 512 1024 2048 4096 8192
-for wid in 512; do
+for wid in 1024; do
   for lr in "${LRS[@]}"; do 
     echo "Running width=$wid, lr=$lr, dim=32"
     $PYTHON -m scripts.MLP_sp \
       --width "$wid" \
       --lr "$lr" \
       --epochs 10 \
-      --bs 1000 \
-      --mini_bs 1000 \
+      --bs 500 \
+      --mini_bs 500 \
       --noise 2 \
       --cifar_data CIFAR10 \
       --clipping_mode BK-MixOpt \
