0d897047c532488078d8cd880e7ea81b30f05638
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-23 16:03:41 -0700

    Update MLP_sp.py

--- scripts/MLP_sp.py
+++ scripts/MLP_sp.py
@@ -313,7 +313,7 @@ def main(args):
 
     logger = ExecutionLogger(args.log_path)
     # logger.log(log2lr=args.lr, train_loss=train_loss, depth=args.layer, batch=args.bs, sigma=args.noise)
-    logger.log(log2lr=args.lr, train_loss=train_loss, train_acc=train_acc, test_acc=test_acc, width=args.width, batch=args.bs, sigma=noise)
+    logger.log(log2lr=args.lr, train_loss=train_loss, train_acc=train_acc, test_acc=test_acc, width=args.width, batch=args.bs, sigma=args.noise)
 
 
 from fastDP import PrivacyEngine 
