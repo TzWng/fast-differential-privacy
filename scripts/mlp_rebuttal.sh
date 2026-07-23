54a69c91ebba7ed627e37e9f47e874338187b291
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-23 15:28:19 -0700

    Update mlp_rebuttal.sh

--- scripts/mlp_rebuttal.sh
+++ scripts/mlp_rebuttal.sh
@@ -62,12 +62,12 @@ for wid in 256 512 1024 2048 4096 8192; do
       --bs 500 \
       --mini_bs 500 \
       --noise 7.067494947837502 \
-      --seed 1 \
+      --seed 5 \
       --cifar_data CIFAR10 \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --dimension 32 \
       --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/MLP_Adam_depth5_s2l_epsilon2_dinfix_dpmup_seed1.txt"
+      --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/MLP_Adam_depth5_s2l_epsilon2_dinfix_dpmup_seed5.txt"
   done
 done
