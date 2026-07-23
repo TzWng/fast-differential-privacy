cd3c7c966e48e06fade288bceb9052806d0214ef
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-23 15:46:52 -0700

    Update mlp_rebuttal.sh

--- scripts/mlp_rebuttal.sh
+++ scripts/mlp_rebuttal.sh
@@ -72,10 +72,9 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-
-LRS=(-15 -14.5 -14 -13.5 -13 -12.5 -12 -11.5 -11) # SGD
+LRS=(-5 -4.5 -4 -3.5 -3 -2.5 -2) # SGD
 # 256 512 1024 2048 4096 8192
-for wid in 2048 4096 8192; do
+for wid in 256 512 1024 2048 4096 8192; do
   for lr in "${LRS[@]}"; do 
     echo "Running width=$wid, lr=$lr, dim=32"
     $PYTHON -m scripts.MLP_sp \
@@ -89,8 +88,31 @@ for wid in 2048 4096 8192; do
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
       --dimension 32 \
-      --optimizer Adam \
-      --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_depth5_s2l_epsilon2_dinfix_dpsp.txt"
+      --optimizer SGD \
+      --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/MLP_SGD_depth5_s2l_epsilon2_dinfix_dpsp.txt"
   done
 done
+
+
+
+# LRS=(-15 -14.5 -14 -13.5 -13 -12.5 -12 -11.5 -11) # SGD
+# # 256 512 1024 2048 4096 8192
+# for wid in 2048 4096 8192; do
+#   for lr in "${LRS[@]}"; do 
+#     echo "Running width=$wid, lr=$lr, dim=32"
+#     $PYTHON -m scripts.MLP_sp \
+#       --width "$wid" \
+#       --lr "$lr" \
+#       --epochs 10 \
+#       --bs 500 \
+#       --mini_bs 500 \
+#       --noise 1.02294921875 \
+#       --cifar_data CIFAR10 \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dimension 32 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs/MLP_Adam_depth5_s2l_epsilon2_dinfix_dpsp.txt"
+#   done
+# done
 
