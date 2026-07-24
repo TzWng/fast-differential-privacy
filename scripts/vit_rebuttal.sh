01c558a618d7456b7617c93f5a940c9ff3bc8c4f
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-23 23:58:48 -0700

    Modify vit_rebuttal.sh to use muon optimizer

--- scripts/vit_rebuttal.sh
+++ scripts/vit_rebuttal.sh
@@ -30,39 +30,41 @@ export PYTHONPATH="$PROJECT_ROOT"
 #   done
 # done
 
-LRS=(-8.5 -8 -7.5 -7)
-SEEDS=(2)
-for seed in "${SEEDS[@]}"; do
-  for s in 3 4 5; do
-    for lr in "${LRS[@]}"; do
-      echo "Running seed=$seed, scale=$s, lr=$lr"
-      $PYTHON -m scripts.vit_unifed \
-        --lr "$lr" \
-        --epochs 3 \
-        --bs 500 \
-        --mini_bs 500 \
-        --epsilon 2 \
-        --noise 2 \
-        --seed "$seed" \
-        --scale "$s" \
-        --clipping_mode BK-MixOpt \
-        --clipping_style layer-wise \
-        --dataset CIFAR10 \
-        --dimension 224 \
-        --optimizer Adam \
-        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
-    done
-  done
-done
 
 
-LRS=(-9 -8.5 -8 -7.5 -7)
-SEEDS=(1 3 4 5)
+# LRS=(-9 -8.5 -8 -7.5 -7)
+# SEEDS=(1 3 4 5)
+# for seed in "${SEEDS[@]}"; do
+#   for s in 1 2 3 4 5; do
+#     for lr in "${LRS[@]}"; do
+#       echo "Running seed=$seed, scale=$s, lr=$lr"
+#       $PYTHON -m scripts.vit_unifed \
+#         --lr "$lr" \
+#         --epochs 3 \
+#         --bs 500 \
+#         --mini_bs 500 \
+#         --epsilon 2 \
+#         --noise 2 \
+#         --seed "$seed" \
+#         --scale "$s" \
+#         --clipping_mode BK-MixOpt \
+#         --clipping_style layer-wise \
+#         --dataset CIFAR10 \
+#         --dimension 224 \
+#         --optimizer Adam \
+#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
+#     done
+#   done
+# done
+
+
+LRS=(-5.75 -5.5 -5.25 -5)
+SEEDS=(1 2 3 4 5)
 for seed in "${SEEDS[@]}"; do
   for s in 1 2 3 4 5; do
     for lr in "${LRS[@]}"; do
       echo "Running seed=$seed, scale=$s, lr=$lr"
-      $PYTHON -m scripts.vit_unifed \
+      $PYTHON -m scripts.vit_muon_sgd \
         --lr "$lr" \
         --epochs 3 \
         --bs 500 \
@@ -75,9 +77,8 @@ for seed in "${SEEDS[@]}"; do
         --clipping_style layer-wise \
         --dataset CIFAR10 \
         --dimension 224 \
-        --optimizer Adam \
-        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
+        --optimizer muon \
+        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_muon_seed${seed}.txt"
     done
   done
 done
-
