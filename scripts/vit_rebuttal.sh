74534a658c16d64186981195a885546bcbb93f8d
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-24 13:21:18 -0700

    Update vit_rebuttal.sh

--- scripts/vit_rebuttal.sh
+++ scripts/vit_rebuttal.sh
@@ -32,39 +32,13 @@ export PYTHONPATH="$PROJECT_ROOT"
 
 
 
-# LRS=(-9 -8.5 -8 -7.5 -7)
-# SEEDS=(3 4 5)
-# for seed in "${SEEDS[@]}"; do
-#   for s in 1 2 3 4 5; do
-#     for lr in "${LRS[@]}"; do
-#       echo "Running seed=$seed, scale=$s, lr=$lr"
-#       $PYTHON -m scripts.vit_unifed \
-#         --lr "$lr" \
-#         --epochs 3 \
-#         --bs 500 \
-#         --mini_bs 500 \
-#         --epsilon 2 \
-#         --noise 2 \
-#         --seed "$seed" \
-#         --scale "$s" \
-#         --clipping_mode BK-MixOpt \
-#         --clipping_style layer-wise \
-#         --dataset CIFAR10 \
-#         --dimension 224 \
-#         --optimizer Adam \
-#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
-#     done
-#   done
-# done
-
-
-LRS=(-6 -5.75 -5.5 -5.25)
+LRS=(-9 -8.5 -8 -7.5 -7)
 SEEDS=(3 4 5)
 for seed in "${SEEDS[@]}"; do
   for s in 1 2 3 4 5; do
     for lr in "${LRS[@]}"; do
       echo "Running seed=$seed, scale=$s, lr=$lr"
-      $PYTHON -m scripts.vit_muon_sgd \
+      $PYTHON -m scripts.vit_unifed \
         --lr "$lr" \
         --epochs 3 \
         --bs 500 \
@@ -77,8 +51,34 @@ for seed in "${SEEDS[@]}"; do
         --clipping_style layer-wise \
         --dataset CIFAR10 \
         --dimension 224 \
-        --optimizer muon \
-        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_muon_seed${seed}.txt"
+        --optimizer Adam \
+        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
     done
   done
 done
+
+
+# LRS=(-6 -5.75 -5.5 -5.25)
+# SEEDS=(3 4 5)
+# for seed in "${SEEDS[@]}"; do
+#   for s in 1 2 3 4 5; do
+#     for lr in "${LRS[@]}"; do
+#       echo "Running seed=$seed, scale=$s, lr=$lr"
+#       $PYTHON -m scripts.vit_muon_sgd \
+#         --lr "$lr" \
+#         --epochs 3 \
+#         --bs 500 \
+#         --mini_bs 500 \
+#         --epsilon 2 \
+#         --noise 2 \
+#         --seed "$seed" \
+#         --scale "$s" \
+#         --clipping_mode BK-MixOpt \
+#         --clipping_style layer-wise \
+#         --dataset CIFAR10 \
+#         --dimension 224 \
+#         --optimizer muon \
+#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_muon_seed${seed}.txt"
+#     done
+#   done
+# done
