446de81607b1d83fece1622fb1e5cda71d63bcf5
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-23 17:48:12 -0700

    Update mlp_rebuttal.sh

--- scripts/mlp_rebuttal.sh
+++ scripts/mlp_rebuttal.sh
@@ -77,54 +77,54 @@ export PYTHONPATH="$PROJECT_ROOT"
 # done
 
 
-LRS=(-10.25 -10.5 -10.75 -11) # SGD
-LRS=(-10.75 -11 -11.25 -11.5 -11.75) # SGD
-SEEDS=(3 4 5)
-for seed in "${SEEDS[@]}"; do
-  for wid in 1024; do
-    for lr in "${LRS[@]}"; do
-      echo "Running width=$wid, seed=$seed, lr=$lr, dim=32"
-      $PYTHON -m scripts.MLP_sp \
-        --width "$wid" \
-        --lr "$lr" \
-        --epochs 10 \
-        --bs 500 \
-        --mini_bs 500 \
-        --noise 1.02294921875 \
-        --seed "$seed" \
-        --cifar_data CIFAR10 \
-        --clipping_mode BK-MixOpt \
-        --clipping_style layer-wise \
-        --dimension 32 \
-        --optimizer Adam \
-        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/MLP_Adam_depth5_s2l_epsilon2_dinfix_dpsp_seed${seed}.txt"
-    done
-  done
-done
-
-
-
-# LRS=(-6.75 -6.5 -6.25 -6 -5.75) # muon
-# LRS=(-7 -7.25 -7.5) # muon
-# SEEDS=(1)
+# LRS=(-10.25 -10.5 -10.75 -11) # SGD
+# LRS=(-10.75 -11 -11.25 -11.5 -11.75) # SGD
+# SEEDS=(3 4 5)
 # for seed in "${SEEDS[@]}"; do
-#   for lr in "${LRS[@]}"; do
-#     for wid in 256; do
+#   for wid in 1024; do
+#     for lr in "${LRS[@]}"; do
 #       echo "Running width=$wid, seed=$seed, lr=$lr, dim=32"
-#       $PYTHON -m scripts.dpmup_sgd \
+#       $PYTHON -m scripts.MLP_sp \
 #         --width "$wid" \
 #         --lr "$lr" \
 #         --epochs 10 \
 #         --bs 500 \
 #         --mini_bs 500 \
-#         --noise 7.067494947837502 \
+#         --noise 1.02294921875 \
 #         --seed "$seed" \
 #         --cifar_data CIFAR10 \
 #         --clipping_mode BK-MixOpt \
 #         --clipping_style layer-wise \
 #         --dimension 32 \
-#         --optimizer muon \
-#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/MLP_muon_depth5_s2l_epsilon2_dinfix_dpmup_seed${seed}.txt"
+#         --optimizer Adam \
+#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/MLP_Adam_depth5_s2l_epsilon2_dinfix_dpsp_seed${seed}.txt"
 #     done
 #   done
 # done
+
+
+
+LRS=(-6.75 -6.5 -6.25 -6 -5.75) # muon
+LRS=(-6.25 -6.5 -6.75 -7 -7.25) # muon
+SEEDS=(1)
+for seed in "${SEEDS[@]}"; do
+  for lr in "${LRS[@]}"; do
+    for wid in 512 1024 2048 4096 8192; do
+      echo "Running width=$wid, seed=$seed, lr=$lr, dim=32"
+      $PYTHON -m scripts.dpmup_sgd \
+        --width "$wid" \
+        --lr "$lr" \
+        --epochs 10 \
+        --bs 500 \
+        --mini_bs 500 \
+        --noise 7.067494947837502 \
+        --seed "$seed" \
+        --cifar_data CIFAR10 \
+        --clipping_mode BK-MixOpt \
+        --clipping_style layer-wise \
+        --dimension 32 \
+        --optimizer muon \
+        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/MLP_muon_depth5_s2l_epsilon2_dinfix_dpmup_seed${seed}.txt"
+    done
+  done
+done
