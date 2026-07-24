11b1b02e69964d7d92ce7b3e6100edd1a47662bd
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-23 22:02:20 -0700

    Create vit_rebuttal.sh

---
+++ scripts/vit_rebuttal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PYTHON=python3.10
+
+PROJECT_ROOT=/content/fast-differential-privacy
+export PYTHONPATH="$PROJECT_ROOT"
+
+
+LRS=(-2 -1.5 -1 -0.5)
+SEEDS=(1 2 3 4 5)
+for seed in "${SEEDS[@]}"; do
+  for s in 1 2; do
+    for lr in "${LRS[@]}"; do
+      echo "Running seed=$seed, scale=$s, lr=$lr"
+      $PYTHON -m scripts.vit_unifed \
+        --lr "$lr" \
+        --epochs 3 \
+        --bs 500 \
+        --mini_bs 500 \
+        --epsilon 2 \
+        --noise 0.9036090970039368 \
+        --seed "$seed" \
+        --scale "$s" \
+        --clipping_mode BK-MixOpt \
+        --clipping_style layer-wise \
+        --dataset CIFAR10 \
+        --dimension 224 \
+        --optimizer SGD \
+        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_SGD_dpmup_seed${seed}.txt"
+    done
+  done
+done
