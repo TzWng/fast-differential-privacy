9c91fd186cc6e0be6f192fa1c3320a63cb0d966e
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-24 22:28:45 -0700

    Update vit_rebuttal.sh

--- scripts/vit_rebuttal.sh
+++ scripts/vit_rebuttal.sh
@@ -5,34 +5,7 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(-2.5 -2 -1.5 -1 -0.5)
-SEEDS=(3 4 5)
-for seed in "${SEEDS[@]}"; do
-  for s in 1 2 3 4 5; do
-    for lr in "${LRS[@]}"; do
-      echo "Running seed=$seed, scale=$s, lr=$lr"
-      $PYTHON -m scripts.vit_unifed \
-        --lr "$lr" \
-        --epochs 3 \
-        --bs 500 \
-        --mini_bs 500 \
-        --epsilon 2 \
-        --noise 2 \
-        --seed "$seed" \
-        --scale "$s" \
-        --clipping_mode BK-MixOpt \
-        --clipping_style layer-wise \
-        --dataset CIFAR10 \
-        --dimension 224 \
-        --optimizer SGD \
-        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_SGD_dpmup_seed${seed}.txt"
-    done
-  done
-done
-
-
-
-# LRS=(-9 -8.5 -8 -7.5 -7)
+# LRS=(-2.5 -2 -1.5 -1 -0.5)
 # SEEDS=(3 4 5)
 # for seed in "${SEEDS[@]}"; do
 #   for s in 1 2 3 4 5; do
@@ -51,11 +24,62 @@ done
 #         --clipping_style layer-wise \
 #         --dataset CIFAR10 \
 #         --dimension 224 \
-#         --optimizer Adam \
-#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
+#         --optimizer SGD \
+#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_SGD_dpmup_seed${seed}.txt"
 #     done
 #   done
 # done
+
+LRS=(-8.75)
+SEEDS=(1 2 3)
+for seed in "${SEEDS[@]}"; do
+  for s in 1; do
+    for lr in "${LRS[@]}"; do
+      echo "Running seed=$seed, scale=$s, lr=$lr"
+      $PYTHON -m scripts.vit_unifed \
+        --lr "$lr" \
+        --epochs 3 \
+        --bs 500 \
+        --mini_bs 500 \
+        --epsilon 2 \
+        --noise 2 \
+        --seed "$seed" \
+        --scale "$s" \
+        --clipping_mode BK-MixOpt \
+        --clipping_style layer-wise \
+        --dataset CIFAR10 \
+        --dimension 224 \
+        --optimizer Adam \
+        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
+    done
+  done
+done
+
+
+LRS=(-9 -8.5 -8 -7.5 -7)
+SEEDS=(4 5)
+for seed in "${SEEDS[@]}"; do
+  for s in 1 2 3 4 5; do
+    for lr in "${LRS[@]}"; do
+      echo "Running seed=$seed, scale=$s, lr=$lr"
+      $PYTHON -m scripts.vit_unifed \
+        --lr "$lr" \
+        --epochs 3 \
+        --bs 500 \
+        --mini_bs 500 \
+        --epsilon 2 \
+        --noise 2 \
+        --seed "$seed" \
+        --scale "$s" \
+        --clipping_mode BK-MixOpt \
+        --clipping_style layer-wise \
+        --dataset CIFAR10 \
+        --dimension 224 \
+        --optimizer Adam \
+        --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
+    done
+  done
+done
 
 
 # LRS=(-6 -5.75 -5.5 -5.25)
