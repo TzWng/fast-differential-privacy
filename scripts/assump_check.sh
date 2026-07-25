7dcdef315472439fc6da9b8b8fe7332973808a1e
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-25 16:59:37 -0700

    Create assump_check.sh for running experiments
    Add a script for assumption checking with placeholders for parameters.

---
+++ scripts/assump_check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PYTHON=python3.10
+
+PROJECT_ROOT=/content/fast-differential-privacy
+export PYTHONPATH="$PROJECT_ROOT"
+
+
+# LRS=(-8.75 -8.5 -8.25 -8 -7.75)
+# SEEDS=(5)
+# for seed in "${SEEDS[@]}"; do
+#   for s in 3 4 5; do
+#     for lr in "${LRS[@]}"; do
+#       echo "Running seed=$seed, scale=$s, lr=$lr"
+#       $PYTHON -m scripts.vit_unifed \
+#         --lr "$lr" \
+#         --epochs 3 \
+#         --bs 500 \
+#         --mini_bs 500 \
+#         --epsilon 2 \
+#         --noise 2 \
+#         --seed "$seed" \
+#         --scale "$s" \
+#         --clipping_mode BK-MixOpt \
+#         --clipping_style layer-wise \
+#         --dataset CIFAR10 \
+#         --dimension 224 \
+#         --optimizer Adam \
+#         --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/Vit_cifar10_Adam_dpmup_seed${seed}.txt"
+#     done
+#   done
+# done
