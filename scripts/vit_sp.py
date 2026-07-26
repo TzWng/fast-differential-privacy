e51fb353a25b99cf0caa8ff987a2fe92e6190361
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-25 17:33:53 -0700

    Update vit_sp.py

--- scripts/vit_sp.py
+++ scripts/vit_sp.py
@@ -17,6 +17,7 @@ def main(args):
 
     # Data
     print('==> Preparing data..')
+    data_root = '/content/drive/MyDrive/DP_muP/data/' 
 
     transformation = torchvision.transforms.Compose([
         torchvision.transforms.Resize(args.dimension),
@@ -25,8 +26,8 @@ def main(args):
     ])
 
     if args.dataset == 'CIFAR10':
-        trainset = torchvision.datasets.CIFAR10(root='data/', train=True, download=True, transform=transformation)
-        testset = torchvision.datasets.CIFAR10(root='data/', train=False, download=True, transform=transformation)
+        trainset = torchvision.datasets.CIFAR10(root=data_root, train=True, download=True, transform=transformation)
+        testset = torchvision.datasets.CIFAR10(root=data_root, train=False, download=True, transform=transformation)
     elif args.dataset == 'CIFAR100':
         trainset = torchvision.datasets.CIFAR100(root='data/', train=True, download=True, transform=transformation)
         testset = torchvision.datasets.CIFAR100(root='data/', train=False, download=True, transform=transformation)
