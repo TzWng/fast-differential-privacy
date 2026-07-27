fca418235ffd97d7964ba14b6153b8d6071a461d
TzWng <91708166+TzWng@users.noreply.github.com>
2026-07-26 17:31:38 -0700

    Update assump_check.sh

--- scripts/assump_check.sh
+++ scripts/assump_check.sh
@@ -5,13 +5,35 @@ PROJECT_ROOT=/content/fast-differential-privacy
 export PYTHONPATH="$PROJECT_ROOT"
 
 
-LRS=(0)
-for s in 5; do
+# LRS=(0)
+# for s in 5; do
+#   for lr in "${LRS[@]}"; do
+#     echo "Running scale=$s, lr=$lr"
+#     $PYTHON -m scripts.vit_sp \
+#       --lr "$lr" \
+#       --epochs 3 \
+#       --bs 250 \
+#       --mini_bs 250 \
+#       --epsilon 2 \
+#       --noise 2 \
+#       --scale "$s" \
+#       --clipping_mode BK-MixOpt \
+#       --clipping_style layer-wise \
+#       --dataset CIFAR100 \
+#       --dimension 224 \
+#       --optimizer Adam \
+#       --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/temp.txt"
+#   done
+# done
+
+
+LRS=(-6)
+for s in 1 2 3 4 5; do
   for lr in "${LRS[@]}"; do
     echo "Running scale=$s, lr=$lr"
-    $PYTHON -m scripts.vit_sp \
+    $PYTHON -m scripts.vit_muon_sgd \
       --lr "$lr" \
-      --epochs 3 \
+      --epochs 1 \
       --bs 250 \
       --mini_bs 250 \
       --epsilon 2 \
@@ -19,9 +41,10 @@ for s in 5; do
       --scale "$s" \
       --clipping_mode BK-MixOpt \
       --clipping_style layer-wise \
-      --dataset CIFAR100 \
+      --dataset CIFAR10 \
       --dimension 224 \
       --optimizer Adam \
       --log_path "/content/drive/MyDrive/DP_muP/logs_rebuttal/temp.txt"
   done
 done
+
